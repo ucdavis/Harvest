0c50bd1f5375ea8095bd62816de959dc5a96cb34
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2021-01-04 16:15:51 -0800

    Configure jest for trx and cobertura reporters

---
+++ Harvest.Web/ClientApp/.rescriptsrc.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+
+  // workaround for CRA not supporting jest's reporters config...
+  jest: config => {
+    const newConfig = {
+      ...config,
+      reporters: [
+        'default',
+        [
+          'jest-trx-results-processor',
+          {
+            outputFile: './output/jest-results.trx',
+            defaultUserName: 'JestRunner'
+          }
+        ]
+      ]
+    }
+    return newConfig;
+  }
+}
