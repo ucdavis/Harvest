dd8c1b70d45efb002495fb0347c38498eed10e7e
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2021-03-09 10:49:57 -0800

    Make client test pass again

--- Harvest.Web/ClientApp/src/App.test.tsx
+++ Harvest.Web/ClientApp/src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Router><App /></Router>);
+  const linkElement = screen.getByText(/Home/i);
   expect(linkElement).toBeInTheDocument();
 });
