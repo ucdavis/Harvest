15c7798cdcd10e2f3846f27eb007b85fcf5e6a8e
Scott Kirkland <srkirkland@gmail.com>
2021-03-12 10:03:35 -0800

    remove extra nav and example of embedded react page

--- Harvest.Web/ClientApp/src/App.tsx
+++ Harvest.Web/ClientApp/src/App.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 
-import { Layout } from "./Layout";
 import { Route } from "react-router-dom";
 import { QuoteContainer } from "./Quotes/QuoteContainer";
 
 function App() {
   return (
-    <Layout>
+    <>
       <Route exact path="/" component={Home} />
       <Route exact path="/home/spa" component={Spa} />
       <Route path="/quote/create/:projectId" component={QuoteContainer} />
-    </Layout>
+    </>
   );
 }
 
