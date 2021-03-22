5287945ba5ab905765615bdf87c16505a6313040
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-03-22 10:01:20 -0700

    Mostly the access denied page

---
+++ Harvest.Web/Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Harvest.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index(int statusCode)
+        {
+            return View(statusCode);
+        }
+    }
+}
