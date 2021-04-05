3c29531112bbcd2fafcf018027010137619ca1b2
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-04-05 09:30:58 -0700

    Migrations

---
+++ Harvest.Core/Migrations/SqlServer/20210405163017_AddRateTable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Harvest.Core.Migrations.SqlServer
+{
+    public partial class AddRateTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    BillingUnit = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    Account = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    EffectiveOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedById = table.Column<int>(type: "int", nullable: false),
+                    UpdatedById = table.Column<int>(type: "int", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rates", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rates_CreatedById",
+                table: "Rates",
+                column: "CreatedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rates_Description",
+                table: "Rates",
+                column: "Description");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rates_Type",
+                table: "Rates",
+                column: "Type");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rates_UpdatedById",
+                table: "Rates",
+                column: "UpdatedById");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rates");
+        }
+    }
+}
