5190fae86a5638df4e85645500b062c2c630bae8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-04-07 09:08:30 -0700

    Migration

---
+++ Harvest.Core/Migrations/Sqlite/20210407160750_LinkUserToRate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Harvest.Core.Migrations.Sqlite
+{
+    public partial class LinkUserToRate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rates_Users_CreatedById",
+                table: "Rates",
+                column: "CreatedById",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rates_Users_UpdatedById",
+                table: "Rates",
+                column: "UpdatedById",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rates_Users_CreatedById",
+                table: "Rates");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rates_Users_UpdatedById",
+                table: "Rates");
+        }
+    }
+}
