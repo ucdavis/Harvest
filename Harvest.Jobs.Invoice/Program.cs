b9d215de78af1cdbf8ec387e99e90096a85526b2
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-04-14 10:04:51 -0700

    wip (based off Peaks)

--- Harvest.Jobs.Invoice/Program.cs
+++ Harvest.Jobs.Invoice/Program.cs
@@ -1,13 +1,41 @@
 ﻿using System;
-using Harvest.Jobs;
+using Harvest.Core.Data;
+using Harvest.Core.Services;
+using Harvest.Jobs.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Harvest.Jobs.Invoice
 {
-    class Program //: JobBase
+    class Program : JobBase
     {
+        //private static ILogger _log;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Configure();
+            var assembyName = typeof(Program).Assembly.GetName();
+            //_log = Log.Logger
+            //    .ForContext("jobname", assembyName.Name)
+            //    .ForContext("jobid", Guid.NewGuid());
+
+            //_log.Information("Running {job} build {build}", assembyName.Name, assembyName.Version);
+
+
+            // setup di
+            var provider = ConfigureServices();
+        }
+
+        private static ServiceProvider ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddOptions();
+            services.AddDbContextPool<AppDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IInvoiceService, InvoiceService>();
+            //services.Configure<SparkpostSettings>(Configuration.GetSection("Sparkpost"));
+
+            return services.BuildServiceProvider();
         }
     }
 }
