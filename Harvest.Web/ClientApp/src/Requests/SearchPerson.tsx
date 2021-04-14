1d8857ca9444aa3c74604a92402b25aafa611bd0
Scott Kirkland <srkirkland@gmail.com>
2021-04-14 11:02:30 -0700

    handle case of no matches

--- Harvest.Web/ClientApp/src/Requests/SearchPerson.tsx
+++ Harvest.Web/ClientApp/src/Requests/SearchPerson.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-import { Label } from "reactstrap";
 import { AsyncTypeahead, Highlighter } from "react-bootstrap-typeahead";
 
 import { User } from "../types";
@@ -19,44 +18,47 @@ export const SearchPerson = (props: Props) => {
     const response = await fetch(`/people/search?query=${query}`);
 
     if (response.ok) {
-      const user: User = await response.json();
+      if (response.status === 204) {
+        setUsers([]); // no content means no match
+      } else {
+        const user: User = await response.json();
 
-      setUsers([user]);
+        setUsers([user]);
+      }
     }
     setIsSearchLoading(false);
   };
 
-  const onSelect = (selected: any) => {};
+  const onSelect = (selected: User[]) => {
+    console.log(selected);
+  };
 
   return (
-    <div>
-      <Label for="searchPeople">Search</Label>
-      <AsyncTypeahead
-        id="searchPeople" // for accessibility
-        isLoading={isSearchLoading}
-        minLength={3}
-        defaultSelected={props.user ? [props.user] : []}
-        placeholder="Search for person by email or kerberos"
-        labelKey={(option: User) => `${option.name} (${option.email})`}
-        filterBy={() => true} // don't filter on top of our search
-        renderMenuItemChildren={(option, propsData, index) => (
+    <AsyncTypeahead
+      id="searchPeople" // for accessibility
+      isLoading={isSearchLoading}
+      minLength={3}
+      defaultSelected={props.user ? [props.user] : []}
+      placeholder="Search for person by email or kerberos"
+      labelKey={(option: User) => `${option.name} (${option.email})`}
+      filterBy={() => true} // don't filter on top of our search
+      renderMenuItemChildren={(option, propsData, index) => (
+        <div>
           <div>
-            <div>
-              <Highlighter key="name" search={propsData.text || ""}>
-                {option.name}
-              </Highlighter>
-            </div>
-            <div>
-              <Highlighter key="email" search={propsData.text || ""}>
-                {option.email}
-              </Highlighter>
-            </div>
+            <Highlighter key="name" search={propsData.text || ""}>
+              {option.name}
+            </Highlighter>
           </div>
-        )}
-        onSearch={onSearch}
-        onChange={onSelect}
-        options={users}
-      />
-    </div>
+          <div>
+            <Highlighter key="email" search={propsData.text || ""}>
+              {option.email}
+            </Highlighter>
+          </div>
+        </div>
+      )}
+      onSearch={onSearch}
+      onChange={onSelect}
+      options={users}
+    />
   );
 };
