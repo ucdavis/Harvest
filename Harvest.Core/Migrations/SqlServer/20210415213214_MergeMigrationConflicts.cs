50745f7c782723797a068818f705e4052647f86a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-04-15 14:32:58 -0700

    Try to rebuild migrations

---
+++ Harvest.Core/Migrations/SqlServer/20210415213214_MergeMigrationConflicts.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Harvest.Core.Migrations.SqlServer
+{
+    public partial class MergeMigrationConflicts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Users_CreatedById",
+                table: "Expenses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedById",
+                table: "Expenses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Users_CreatedById",
+                table: "Expenses",
+                column: "CreatedById",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Users_CreatedById",
+                table: "Expenses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedById",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Users_CreatedById",
+                table: "Expenses",
+                column: "CreatedById",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
