7a085cd2b3c9e44ffef091b003946bc802903e0f
Scott Kirkland <srkirkland@gmail.com>
2021-04-21 13:24:52 -0700

    extract quote totals and show on quote input as well as display

---
+++ Harvest.Web/ClientApp/src/Quotes/QuoteTotals.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Card, CardBody, CardHeader, Col, Row } from "reactstrap";
+
+import { QuoteContent } from "../types";
+import { formatCurrency } from '../Util/NumberFormatting';
+
+
+interface Props {
+  quote: QuoteContent;
+}
+
+export const QuoteTotals = (props: Props) => {
+  return (
+    <Card className="card-project-totals">
+      <CardHeader>Project Totals</CardHeader>
+      <CardBody>
+        <div id="total">
+          <h6>Project Totals</h6>
+          <hr />
+          <Row>
+            <Col xs="10" sm="10">
+              <div>Acreage Fees</div>
+            </Col>
+            <Col xs="2" sm="2">
+              ${formatCurrency(props.quote.acreageTotal)}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="10" sm="10">
+              <div>Labor</div>
+            </Col>
+            <Col xs="2" sm="2">
+              ${formatCurrency(props.quote.laborTotal)}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="10" sm="10">
+              <div>Equipment</div>
+            </Col>
+            <Col xs="2" sm="2">
+              ${formatCurrency(props.quote.equipmentTotal)}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="10" sm="10">
+              <div>Materials / Other</div>
+            </Col>
+            <Col xs="2" sm="2">
+              ${formatCurrency(props.quote.otherTotal)}
+            </Col>
+          </Row>
+          <Row className="total-row">
+            <Col xs="10" sm="10">
+              <h6>Total Cost</h6>
+            </Col>
+            <Col xs="2" sm="2">
+              <span>${formatCurrency(props.quote.grandTotal)}</span>
+            </Col>
+          </Row>
+        </div>
+      </CardBody>
+    </Card>
+  );
+};
