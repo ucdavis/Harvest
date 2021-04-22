2d4e6c7d796453f3e2e393308bb92740ada9f059
Scott Kirkland <srkirkland@gmail.com>
2021-04-22 10:52:46 -0700

    add new columns to project

--- Harvest.Core/Domain/Project.cs
+++ Harvest.Core/Domain/Project.cs
@@ -13,15 +13,15 @@ namespace Harvest.Core.Domain
     {
         [Key]
         public int Id { get; set; }
-        
+
         public DateTime Start { get; set; }
-        
+
         public DateTime End { get; set; }
-        
+
         // TODO: make this biggere in case we have a bunch of crops
         [StringLength(50)]
         public string Crop { get; set; }
-        
+
         [MaxLength]
         public string Requirements { get; set; }
 
@@ -29,44 +29,52 @@ namespace Harvest.Core.Domain
 
         public int? AcreageRateId { get; set; }
         public Rate AcreageRate { get; set; }
-        
+
         [StringLength(200)]
         public string Name { get; set; }
-        
+
         public int PrincipalInvestigatorId { get; set; }
-        
+
         public User PrincipalInvestigator { get; set; }
 
         public Geometry Location { get; set; }
-        
+
         [StringLength(50)]
         public string LocationCode { get; set; }
-        
+
         public int? QuoteId { get; set; }
-        
+
         public Quote Quote { get; set; }
-        // public decimal AcreagePerMonth  { get; set; }
-        
+
+        // Change request will refer back to original project
+        public int? OriginalProjectId { get; set; }
+
+        [JsonIgnore]
+        public Project OriginalProject { get; set; }
+
         [Required]
         [Display(Name = "Quote Total")]
         public decimal QuoteTotal { get; set; }
-        
+
         [Required]
         [Display(Name = "Charged Total")]
         public decimal ChargedTotal { get; set; }
-        
+
         [Required]
         public int CreatedById { get; set; }
 
         public DateTime CreatedOn { get; set; }
-        
+
         [StringLength(50)]
         public string Status { get; set; }
-        
+
         [Required]
         [Display(Name = "Current Account Version")]
         public int CurrentAccountVersion { get; set; }
-        
+
+        [Required]
+        public bool IsApproved { get; set; } = true;
+
         [Required]
         // need or can we filter?
         [Display(Name = "Is Active")]
@@ -114,13 +122,12 @@ namespace Harvest.Core.Domain
                 .WithMany(p => p.Quotes)
                 .HasForeignKey(q => q.ProjectId)
                 .OnDelete(DeleteBehavior.Restrict);
-
         }
         public class Statuses
         {
             public const string Requested = "Requested";
             public const string PendingAccountApproval = "Pending Account Approval";
-            public const string Active    = "Active";
+            public const string Active = "Active";
             public const string Completed = "Completed";
 
             public static List<string> TypeList = new List<string>
