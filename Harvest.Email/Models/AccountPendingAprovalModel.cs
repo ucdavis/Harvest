77af697f1963f3e5db9093a0578d12ae5d51dece
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-04-27 08:28:26 -0700

    Email for Accounts needing approval.

---
+++ Harvest.Email/Models/AccountPendingAprovalModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Harvest.Email.Models
+{
+    public class AccountPendingApprovalModel
+    {
+        public string PI { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectEnd { get; set; }
+        public string ProjectStart { get; set; }
+
+        public string ButtonUrl { get; set; }
+        public List<AccountsForApprovalModel> AccountsList { get; set; }
+        public string MjmlListOnly { get; private set; }
+
+        /// <summary>
+        /// Sets model values when running TestController.TestBody
+        /// </summary>
+        public void InitForMjml()
+        {
+            PI                  = "@Model.PI";
+            ProjectName         = "@Model.ProjectName";
+            ProjectStart        = "@Model.ProjectStart";
+            ProjectEnd          = "@Model.ProjectEnd";
+            ButtonUrl           = "@Model.ButtonUrl";
+            var sb = new StringBuilder();
+            sb.AppendLine("@foreach (var item in Model.AccountsList){");
+            sb.AppendLine("<tr>");
+            sb.AppendLine("<td style=\"padding: 0 15px 0 0;\">@item.Account</td>");
+            sb.AppendLine("<td style=\"padding: 0 15px;\">@item.Name</td>");
+            sb.AppendLine("<td style=\"padding: 0 0 0 15px;\">@item.Percent</td>");
+            sb.AppendLine("</tr>");
+            sb.AppendLine("}");
+            MjmlListOnly = sb.ToString();
+        }
+
+    }
+    public class AccountsForApprovalModel
+    {
+        public string Account { get; set; }
+        public string Name { get; set; }
+        public string Percent { get; set; }
+    }
+}
--- Harvest.Web/Controllers/TestController.cs
+++ Harvest.Web/Controllers/TestController.cs
@@ -40,9 +40,14 @@ namespace Harvest.Web.Controllers
 
             //var results = await _emailBodyService.RenderBody("/Views/Emails/ChangeRequest_mjml.cshtml", model);
 
-            var model = new TestEmailModel();
+            //var model = new TestEmailModel();
+            //model.InitForMjml();
+            //var results = await _emailBodyService.RenderBody("/Views/Emails/TestEmail_mjml.cshtml", model);
+
+            var model = new AccountPendingApprovalModel();
             model.InitForMjml();
-            var results = await _emailBodyService.RenderBody("/Views/Emails/TestEmail_mjml.cshtml", model);
+
+            var results = await _emailBodyService.RenderBody("/Views/Emails/AccountPendingApproval_mjml.cshtml", model);
 
             return Content(results);
         }
@@ -70,16 +75,31 @@ namespace Harvest.Web.Controllers
 
             //var emailBody = await _emailBodyService.RenderBody("/Views/Emails/QuoteDecisionEmail.cshtml", model);
 
-            var model = new TestEmailModel();
-            model.Name = "Jason";
-            model.MyList = new List<string>();
-            model.MyList.Add("Test Line 1");
-            model.MyList.Add("Test Line 2");
-            model.MyList.Add("For The WIN");
-            model.MyList.Add("Last Line");
-            var emailBody = await _emailBodyService.RenderBody("/Views/Emails/TestEmail.cshtml", model);
+            //var model = new TestEmailModel();
+            //model.Name = "Jason";
+            //model.MyList = new List<string>();
+            //model.MyList.Add("Test Line 1");
+            //model.MyList.Add("Test Line 2");
+            //model.MyList.Add("For The WIN");
+            //model.MyList.Add("Last Line");
+            //var emailBody = await _emailBodyService.RenderBody("/Views/Emails/TestEmail.cshtml", model);
 
-            await _notificationService.SendNotification(new string[]{ user.Email }, emailBody, "A quote is ready for your review/approval for your harvest project.", "Harvest Notification - Quote Ready");
+            //await _notificationService.SendNotification(new string[]{ user.Email }, emailBody, "A quote is ready for your review/approval for your harvest project.", "Harvest Notification - Quote Ready");
+
+            var model = new AccountPendingApprovalModel();
+            model.PI = user.NameAndEmail;
+            model.ProjectName = "Jason's Awesome Project";
+            model.ProjectStart = DateTime.UtcNow.ToPacificTime().Date.Format("d");
+            model.ProjectEnd = DateTime.UtcNow.AddYears(2).ToPacificTime().Date.Format("d");
+            model.ButtonUrl = "https://harvest.caes.ucdavis.edu";
+            model.AccountsList = new List<AccountsForApprovalModel>();
+            model.AccountsList.Add(new AccountsForApprovalModel() { Account = "3-CRU9033", Name = "COMPUTING RESOURCES UNIT- GETCHELL", Percent = "75%" });
+            model.AccountsList.Add(new AccountsForApprovalModel() { Account = "3-APSNFDS", Name = "PS:FIELD:DAVIS SHOP ACCOUNT", Percent = "15%" });
+            model.AccountsList.Add(new AccountsForApprovalModel() { Account = "3-RRCNTRY", Name = "RUSSELL RANCH CENTURY PROJECT", Percent = "10%" });
+
+            var emailBody = await _emailBodyService.RenderBody("/Views/Emails/AccountPendingApproval.cshtml", model);
+            await _notificationService.SendNotification(new string[] { user.Email }, emailBody, "A quote is ready for your review/approval for your harvest project.", "Harvest Notification - Accounts Need Approval");
+
             return Content("Done. Maybe. Well, possibly. If you don't get it, check the settings.");
         }
 
