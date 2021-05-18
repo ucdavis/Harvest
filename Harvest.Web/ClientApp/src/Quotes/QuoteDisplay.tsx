516fb5cf92d17ca57b940728d768f131ef260801
Cal <cydoval@mac.com>
2021-05-18 13:57:53 -0600

    quote display

--- Harvest.Web/ClientApp/src/Quotes/QuoteDisplay.tsx
+++ Harvest.Web/ClientApp/src/Quotes/QuoteDisplay.tsx
@@ -14,10 +14,13 @@ export const QuoteDisplay = (props: Props) => {
   return (
     <div>
       <h1>Quote</h1>
-      {quote.acreageRateDescription}: {quote.acres} @{" "}
-      {formatCurrency(quote.acreageRate)} = $
-      {formatCurrency(quote.acreageTotal)}
-      <hr />
+      <p>
+        <b>
+          {quote.acreageRateDescription}: {quote.acres} @{" "}
+          {formatCurrency(quote.acreageRate)} = $
+          {formatCurrency(quote.acreageTotal)}
+        </b>
+      </p>
       {quote.activities.map((activity) => (
         <div
           className="quote-actvitiy-item card-wrapper mb-4 gray-top"
@@ -25,7 +28,7 @@ export const QuoteDisplay = (props: Props) => {
         >
           <div className="card-header">
             <h4>
-              <span className="primary-color bold-font">{activity.name}</span> |
+              <span className="primary-color bold-font">{activity.name}</span> •
               Activity Total: ${formatCurrency(activity.total)}
             </h4>
           </div>
