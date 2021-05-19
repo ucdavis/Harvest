51fe8f053d51b953c015eedafaaa4f8e0e5b9293
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-05-19 10:07:24 -0700

    Migration

---
+++ Harvest.Core/Migrations/SqlServer/20210519170608_ExpenseAprovalFlag.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Harvest.Core.Migrations.SqlServer
+{
+    public partial class ExpenseAprovalFlag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Approved",
+                table: "Expenses",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Approved",
+                table: "Expenses");
+        }
+    }
+}
