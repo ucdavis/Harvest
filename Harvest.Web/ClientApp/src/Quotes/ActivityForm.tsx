543dddcbcde388aa734b01a432f48b08a8f49ff8
Cal <cydoval@mac.com>
2021-06-01 11:51:18 -0600

    small tweaks

--- Harvest.Web/ClientApp/src/Quotes/ActivityForm.tsx
+++ Harvest.Web/ClientApp/src/Quotes/ActivityForm.tsx
@@ -55,8 +55,9 @@ export const ActivityForm = (props: Props) => {
   return (
     <div className="card-wrapper mb-4 no-green">
       <div className="card-content">
-        <div className="row justify-content-between align-items-center">
+        <div className="row justify-content-between align-items-end">
           <div className="col-md-8">
+            <label> Activity Name</label>
             <Input
               type="text"
               id="activityName"
@@ -70,7 +71,10 @@ export const ActivityForm = (props: Props) => {
             ></Input>
           </div>
           <div className="col-md-4">
-            <button className="btn btn-link btn-sm" onClick={() => props.deleteActivity(props.activity)}>
+            <button
+              className="btn btn-link btn-sm"
+              onClick={() => props.deleteActivity(props.activity)}
+            >
               Remove activity <FontAwesomeIcon icon={faMinusCircle} />
             </button>
           </div>
