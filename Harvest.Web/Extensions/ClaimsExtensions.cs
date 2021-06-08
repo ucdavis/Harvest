491e820d0614ba7376ca233121f1a5c552fe8bdc
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-06-08 10:17:56 -0700

    imports and updated code to get more info from claims

--- Harvest.Web/Extensions/ClaimsExtensions.cs
+++ Harvest.Web/Extensions/ClaimsExtensions.cs
@@ -9,6 +9,7 @@ namespace Harvest.Web.Extensions
 {
     public static class ClaimsExtensions
     {
+        public const string IamIdClaimType = "ucdPersonIAMID";
         public static string GetNameClaim(this ClaimsPrincipal claimsPrincipal)
         {
             if (claimsPrincipal == null || !claimsPrincipal.Identity.IsAuthenticated) return string.Empty;
@@ -18,17 +19,20 @@ namespace Harvest.Web.Extensions
             return nameClaim != null ? nameClaim.Value : claimsPrincipal.Identity.Name;
         }
 
-        //public static User GetUserInfo(this ClaimsPrincipal claimsPrincipal)
-        //{
-        //    if (claimsPrincipal == null || !claimsPrincipal.Identity.IsAuthenticated) return null;
-
-        //    return new User
-        //    {
-        //        FirstName = claimsPrincipal.FindFirstValue(ClaimTypes.GivenName),
-        //        LastName = claimsPrincipal.FindFirstValue(ClaimTypes.Surname),
-        //        Email = claimsPrincipal.FindFirstValue(ClaimTypes.Email),
-        //        Kerberos = claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier) //???
-        //    };
-        //}
+        public static User GetUserInfo(this ClaimsPrincipal claimsPrincipal)
+        {
+            if (claimsPrincipal == null || !claimsPrincipal.Identity.IsAuthenticated) return null;
+            var iam = claimsPrincipal.Claims.SingleOrDefault(a => a.Type == IamIdClaimType);
+            //Need this when I was testing my own login. was blank if I just used the first value
+            var first = claimsPrincipal.Claims.FirstOrDefault(a => a.Type == ClaimTypes.GivenName && !string.IsNullOrWhiteSpace(a.Value));
+            return new User
+            {
+                FirstName = first?.Value,
+                LastName = claimsPrincipal.FindFirstValue(ClaimTypes.Surname),
+                Email = claimsPrincipal.FindFirstValue(ClaimTypes.Email),
+                Kerberos = claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier), //???
+                Iam = iam?.Value
+            };
+        }
     }
 }
