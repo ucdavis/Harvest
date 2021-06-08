959d111e4cc87e9ec4907b9e3be7684d20d4001c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-06-08 09:30:12 -0700

    User name instead of login id
    resolves #206

---
+++ Harvest.Web/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Harvest.Core.Domain;
+
+namespace Harvest.Web.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static string GetNameClaim(this ClaimsPrincipal claimsPrincipal)
+        {
+            if (claimsPrincipal == null || !claimsPrincipal.Identity.IsAuthenticated) return string.Empty;
+
+            var nameClaim = claimsPrincipal.FindFirst("name");
+
+            return nameClaim != null ? nameClaim.Value : claimsPrincipal.Identity.Name;
+        }
+
+        //public static User GetUserInfo(this ClaimsPrincipal claimsPrincipal)
+        //{
+        //    if (claimsPrincipal == null || !claimsPrincipal.Identity.IsAuthenticated) return null;
+
+        //    return new User
+        //    {
+        //        FirstName = claimsPrincipal.FindFirstValue(ClaimTypes.GivenName),
+        //        LastName = claimsPrincipal.FindFirstValue(ClaimTypes.Surname),
+        //        Email = claimsPrincipal.FindFirstValue(ClaimTypes.Email),
+        //        Kerberos = claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier) //???
+        //    };
+        //}
+    }
+}
