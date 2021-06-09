5f3aefa4f8fe63be5bc32de02704c7a64c542daf
Cal <cydoval@mac.com>
2021-06-09 11:42:52 -0600

    changes to activity form2

--- Harvest.Web/ClientApp/src/Quotes/ActivityForm.tsx
+++ Harvest.Web/ClientApp/src/Quotes/ActivityForm.tsx
@@ -68,6 +68,32 @@ export const ActivityForm = (props: Props) => {
                 <button className="btn btn-link btn-sm">
                   Adjust Year <FontAwesomeIcon icon={faCalendarWeek} />
                 </button>
+                <div className="btn-group">
+                  <button
+                    type="button"
+                    className="btn btn-danger dropdown-toggle"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Action
+                  </button>
+                  <div className="dropdown-menu">
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="#">
+                      Separated link
+                    </a>
+                  </div>
+                </div>
                 <button className="btn btn-link btn-sm">
                   Duplicate Activity <FontAwesomeIcon icon={faCopy} />
                 </button>
