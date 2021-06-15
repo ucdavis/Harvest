15a029b93a40593eca770552a103bfbc09c7ae04
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-06-15 14:39:07 -0700

    Info text

--- Harvest.Core/Models/AccessCodes.cs
+++ Harvest.Core/Models/AccessCodes.cs
@@ -8,6 +8,9 @@ namespace Harvest.Core.Models
         public const string FieldManagerAccess = "FieldManagerAccess";
         public const string SupervisorAccess = "SupervisorAccess";
         public const string WorkerAccess = "WorkerAccess";
+        /// <summary>
+        /// When using this Authentication, the project id must be in either the projectId or id parameters. It will look for the projectId first.
+        /// </summary>
         public const string PrincipalInvestigatorProjectMustBeIdOrProjectId = "PrincipalInvestigator";
     }
 }
