0e6081819b2a349333f273aa3f1d501c0e24857e
Scott Kirkland <srkirkland@gmail.com>
2021-06-17 11:43:37 -0700

    add options and refactor

--- Harvest.Core/Services/FileService.cs
+++ Harvest.Core/Services/FileService.cs
@@ -1,35 +1,54 @@
 using System;
 using Azure.Storage.Blobs;
 using Azure.Storage.Sas;
+using Harvest.Core.Models.Settings;
+using Microsoft.Extensions.Options;
 
 namespace Harvest.Core.Services
 {
-    public class FileService
+    public interface IFileService
     {
-        public FileService()
-        {
+        Uri GetDownloadUrl(string container = "upload");
+        Uri GetUploadUrl(string container = "upload");
+    }
 
+    public class FileService : IFileService
+    {
+        private StorageSettings _storageSettings;
+
+        public FileService(IOptions<StorageSettings> storageSettings)
+        {
+            _storageSettings = storageSettings.Value;
         }
 
-        public Uri GetUploadUrl(string container = "upload")
+        public Uri GetDownloadUrl(string container)
         {
-            var endpoint = "TODO:AUTH";
+            return GetServiceSasUriForContainer(container, BlobContainerSasPermissions.Read);
+        }
+
+        public Uri GetUploadUrl(string container)
+        {
+            return GetServiceSasUriForContainer(container, BlobContainerSasPermissions.Write);
+        }
+
+        private Uri GetServiceSasUriForContainer(string container, BlobContainerSasPermissions permissions = BlobContainerSasPermissions.Read, DateTimeOffset? expiresOn = null)
+        {
+            // var endpoint = "TODO:AUTH";
 
             var client = new BlobContainerClient(endpoint, container);
 
             // Check whether this BlobContainerClient object has been authorized with Shared Key.
             if (client.CanGenerateSasUri)
             {
-                // Create a SAS token that's valid for one hour.
                 BlobSasBuilder sasBuilder = new BlobSasBuilder()
                 {
                     BlobContainerName = client.Name,
                     Resource = "c"
                 };
 
-                // allow writing for 1 hr
-                sasBuilder.ExpiresOn = DateTimeOffset.UtcNow.AddHours(1);
-                sasBuilder.SetPermissions(BlobContainerSasPermissions.Write);
+
+                sasBuilder.ExpiresOn = expiresOn.HasValue ? expiresOn.Value : DateTimeOffset.UtcNow.AddHours(1);
+                sasBuilder.SetPermissions(permissions);
 
                 return client.GenerateSasUri(sasBuilder);
             }
