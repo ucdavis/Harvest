7874974b0212f1c45ee5354e62d55379a7105fbb
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-06-17 13:34:19 -0700

    Add a Task table

---
+++ Harvest.Core/Domain/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harvest.Core.Domain
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+        public int? InvoiceId { get; set; }
+        public Invoice Invoice { get; set; }
+
+        [Required]
+        public int CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public int UpdatedById { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+        [Required]
+        public string Requirements { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public string Status { get; set; } = "Created";
+        public bool Completed { get; set; } = false;
+        
+    }
+}
