0840d4b233b314fdde4874690de3e28ecb74a0f7
Scott Kirkland <srkirkland@gmail.com>
2021-06-21 13:32:40 -0700

    new project attachment class and attachment base class

---
+++ Harvest.Core/Domain/ProjectAttachment.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Harvest.Core.Domain
+{
+    public class ProjectAttachment : AttachmentBase
+    {
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        internal static void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProjectAttachment>().HasIndex(a => a.ProjectId);
+            modelBuilder.Entity<ProjectAttachment>().HasIndex(a => a.CreatedOn);
+            modelBuilder.Entity<ProjectAttachment>().HasIndex(a => a.CreatedById);
+            modelBuilder.Entity<ProjectAttachment>().Property(a => a.FileName).HasMaxLength(250);
+            modelBuilder.Entity<ProjectAttachment>().Property(a => a.ContentType).HasMaxLength(250);
+            modelBuilder.Entity<ProjectAttachment>().Property(a => a.Identifier).HasMaxLength(128);
+        }
+    }
+}
