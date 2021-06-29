54d0d36d1e93781b14f182729d1032d2084536ee
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2021-06-29 11:40:02 -0700

    Move functions form FormikBag to useWrappedFormik

--- Harvest.Web/ClientApp/src/Validation/index.tsx
+++ Harvest.Web/ClientApp/src/Validation/index.tsx
@@ -1,5 +1,5 @@
-﻿import React from "react";
-import { useFormik, FormikConfig, FormikValues, FieldHelperProps, FieldInputProps, FieldMetaProps, FormikErrors, FormikState, FormikTouched } from "formik";
+﻿import React, { useMemo } from "react";
+import { useFormik, FormikConfig, FormikValues } from "formik";
 
 //hack to get return type of useFormic()
 class FormikBagInferer<T> {
@@ -12,50 +12,59 @@ class FormikBagInferer<T> {
 }
 export type RootFormikBag<T> = ReturnType<FormikBagInferer<T>['wrapped']>;
 
+// EXAMPLE: path of root.values.activities[0].workItems[1] will be "activities.0.workitems.1"
+const getPath = (from: object, to: object) => {
+  return traverse(from).join(".");
+
+  function traverse(traverseFrom: object, path: string[] = []): string[] {
+    for (const [key, value] of Object.entries(traverseFrom).filter(e => !!e[1] && typeof e[1] === "object")) {
+      if (value === to) {
+        return [...path, key];
+      }
+      const newPath = traverse(value, [...path, key]);
+      if (newPath.length > 0) {
+        return newPath;
+      }
+    }
+    return [];
+  }
+}
+
 //wrapper hook so that, most of the time, we'll only have to deal with FormikBag
 export function useWrappedFormik<Values extends FormikValues = FormikValues>(props: FormikConfig<Values>) {
   const rootFormikBag = useFormik(props);
-  const formikBag = new FormikBag(rootFormikBag, formik => formik.values);
+
+  const rootGetValues = (root: RootFormikBag<Values>) => root.values;
+  
+  const getNestedBag = <TNested extends object>(getValues: (values: Values) => TNested): FormikBag<Values, TNested> => {
+    const newGetValues = (nestedRoot: RootFormikBag<Values>) => getValues(rootGetValues(nestedRoot));
+    const newGetNestedBag = <TNextNested extends object>(nextGetValues: (values: TNested) => TNextNested): FormikBag<Values, TNextNested> =>
+      getNestedBag<TNextNested>(rootValues => nextGetValues(getValues(rootValues)))
+    const path = getPath(rootGetValues(rootFormikBag), newGetValues(rootFormikBag));
+    return new FormikBag<Values, TNested>(rootFormikBag, newGetValues, newGetNestedBag, path);
+  }
+
+
+  const formikBag = new FormikBag(rootFormikBag, rootGetValues, getNestedBag, "");
   return formikBag;
 }
 
 //wrapper to help reduce a bunch of boilerplate when dealing with nested objects
 export class FormikBag<TRoot extends object, TNested extends object = TRoot> implements Pick<RootFormikBag<TRoot>, "getFieldMeta"> {
-  constructor(root: RootFormikBag<TRoot>, getValues: (root: RootFormikBag<TRoot>) => TNested) {
+  constructor(root: RootFormikBag<TRoot>, getValues: (root: RootFormikBag<TRoot>) => TNested,
+    getNestedBag: <TNextNested extends object> (getValues: (values: TNested) => TNextNested) => FormikBag<TRoot, TNextNested>,
+    path: string) {
     this.root = root;
     this.getValues = getValues;
-    this.path = this.getPath(root.values, getValues(root));
+    this.path = path;
+    this.getNestedBag = getNestedBag;
   }
 
   private getValues: (root: RootFormikBag<TRoot>) => TNested;
   root: RootFormikBag<TRoot>;
   path: string;
   fullPath(name: string) { return !this.path || this.path === "" ? name : `${this.path}.${name}`; }
-  getNestedBag<TNextNested extends object>(getValues: (values: TNested) => TNextNested) {
-    const rootGetValues = (nestedRoot: RootFormikBag<TRoot>) => this.getValues(nestedRoot);
-    const root = this.root;
-    const newGetValues = (nestedRoot: RootFormikBag<TRoot>) => getValues(rootGetValues(nestedRoot));
-    return new FormikBag<TRoot, TNextNested>(root, newGetValues);
-  }
-
-  // EXAMPLE: root => root.values.activities[0].workItems[1] will result in path "activities.0.workitems.1"
-  // TODO: performance isn't great, so maybe try memoizing
-  private getPath(from: object, to: object) {
-    return traverse(from).join(".");
-
-    function traverse(traverseFrom: object, path: string[] = []): string[] {
-      for (const [key, value] of Object.entries(traverseFrom).filter(e => !!e[1] && typeof e[1] === "object")) {
-        if (value === to) {
-          return [...path, key];
-        }
-        const newPath = traverse(value, [...path, key]);
-        if (newPath.length > 0) {
-          return newPath;
-        }
-      }
-      return [];
-    }
-  }
+  getNestedBag: <TNextNested extends object>(getValues: (values: TNested) => TNextNested) => FormikBag<TRoot, TNextNested>;
 
   get handleBlur() { return this.root.handleBlur }
   get handleChange() { return this.root.handleChange }
