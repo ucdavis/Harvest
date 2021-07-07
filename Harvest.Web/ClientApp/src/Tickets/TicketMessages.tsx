f02835859fd3958b1c9d949936d7dab1de314618
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-07-07 08:53:57 -0700

    cleanup

--- Harvest.Web/ClientApp/src/Tickets/TicketMessages.tsx
+++ Harvest.Web/ClientApp/src/Tickets/TicketMessages.tsx
@@ -8,22 +8,17 @@ interface Props {
 export const TicketMessages = (props: Props) => {
   const ticketMessages = useMemo(() => props.messages, [props.messages]);
 
-  if (ticketMessages === undefined || ticketMessages.length === 0) {
-    return (
-      <div>
-        <h2>Conversation</h2>
-        <p>No Messages Yet!!!</p>
-      </div>
-    );
-  }
-
   return (
     <div>
-          <h2>Conversation</h2>
-          {ticketMessages.map((ticketMessage) => (
-              <p>{ticketMessage.message} from {ticketMessage.createdBy.name}</p>
-          ))}
-
+      <h2>Conversation</h2>
+      {ticketMessages === undefined || ticketMessages.length === 0 ? (
+        <p> No Messages Yet!!!</p>
+      ) : null}
+      {ticketMessages.map((ticketMessage) => (
+        <p>
+          {ticketMessage.message} from {ticketMessage.createdBy.name}
+        </p>
+      ))}
     </div>
   );
 };
