3e2678e13b1964ad67ebe61f4124e44c7f036461
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-07-15 10:02:30 -0700

    code not needed

--- Harvest.Web/ClientApp/src/Tickets/TicketWorkNotesEdit.tsx
+++ Harvest.Web/ClientApp/src/Tickets/TicketWorkNotesEdit.tsx
@@ -26,7 +26,6 @@ export const TicketWorkNotesEdit = (props: Props) => {
       );
 
       if (response.ok) {
-          await response.json();
           alert("Work notes saved.");
       } else {
           alert("Something went wrong, please try again");
