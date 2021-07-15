31de4cbab2f5395eabf929d56cd5b50bd942be3e
Scott Kirkland <srkirkland@gmail.com>
2021-07-15 11:17:00 -0700

    get a little fancy with dynamic scripts

--- Harvest.Web/Views/Shared/Components/DynamicScripts/DynamicScripts.cs
+++ Harvest.Web/Views/Shared/Components/DynamicScripts/DynamicScripts.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -19,10 +22,47 @@ namespace Harvest.Web.Views.Shared.Components.DynamicScripts
         {
             var files = _fileProvider.GetDirectoryContents("ClientApp/build/static/js");
 
-            // Get all JS files
-            // TODO: We might not actually need all of them since some of the chunks can be loaded later.  But it shouldn't hurt for now.
-            var fileNames = files.Where(f => Regex.IsMatch(f.Name, "^.*\\.js$")).OrderByDescending(f => f.Name).Select(f => f.Name);
-            return View(await Task.FromResult(fileNames.ToArray()));
+            /* 
+             * Get all JS files in the static build folder
+             * Should include the following files:
+             * - main.*.js
+             * - 2.[hash].chunk.js
+             * - runtime-main.*.js
+             * - [number].[hash].chunk.js (for each additional chunk)
+
+             =======
+             We would like to inline the runtime-main file, then load 2.*.chunk.js, and then main.*.chunk.js
+            */
+
+            var model = new DynamicScriptModel();
+            var runtimeFile = files.Where(f => Regex.IsMatch(f.Name, "^.*runtime-main.*\\.js$")).First();
+
+            // read the entire runtime-main file
+            using (var reader = runtimeFile.CreateReadStream())
+            {
+                using (var streamReader = new StreamReader(reader))
+                {
+                    model.Runtime = await streamReader.ReadToEndAsync();
+                }
+            }
+
+            var scripts = new List<string>();
+
+            // read the 2.*.chunk.js files (maybe there is always just one?)
+            scripts.AddRange(files.Where(f => Regex.IsMatch(f.Name, "^.*2\\..*\\.chunk\\.js$")).Select(f => f.Name));
+
+            // now add in main.*.chunk.js
+            scripts.AddRange(files.Where(f => Regex.IsMatch(f.Name, "^.*main\\..*\\.chunk\\.js$")).Select(f => f.Name));
+
+            model.Scripts = scripts.ToArray();
+
+            return View(model);
         }
+    }
+
+    public class DynamicScriptModel
+    {
+        public string[] Scripts { get; set; }
+        public string Runtime { get; set; }
     }
 }
