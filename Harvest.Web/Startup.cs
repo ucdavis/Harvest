7eb68ec50e199682b96a688bff010e5b5a9c79cf
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2021-07-15 12:17:06 -0700

    Document access levels

--- Harvest.Web/Startup.cs
+++ Harvest.Web/Startup.cs
@@ -91,10 +91,15 @@ namespace Harvest.Web
             {
                 return accessCode switch
                 {
+                    // System can access anything
                     AccessCodes.SystemAccess => new[] { Role.Codes.System },
+                    // FieldManager can access anything restricted to FieldManager, Supervisor, Worker or PI roles
                     AccessCodes.FieldManagerAccess => new[] { Role.Codes.FieldManager },
+                    // Supervisor can access anything restricted to Supervisor, Worker or PI roles
                     AccessCodes.SupervisorAccess => new[] { Role.Codes.Supervisor, Role.Codes.FieldManager },
+                    // Worker can access anything restricted to Worker role
                     AccessCodes.WorkerAccess => new[] { Role.Codes.Worker, Role.Codes.Supervisor, Role.Codes.FieldManager },
+                    // PI can access anything restricted to PI role
                     AccessCodes.PrincipalInvestigator => new[] { Role.Codes.PI, Role.Codes.Supervisor, Role.Codes.FieldManager },
                     _ => throw new ArgumentException($"{nameof(accessCode)} is not a valid {nameof(AccessCodes)} constant")
                 };
