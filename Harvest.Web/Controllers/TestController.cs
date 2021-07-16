c57086cdd9cf2f84875735b35de7c2c16850cff1
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-07-16 14:13:27 -0700

    Ticket Reply notification

--- Harvest.Web/Controllers/TestController.cs
+++ Harvest.Web/Controllers/TestController.cs
@@ -38,10 +38,10 @@ namespace Harvest.Web.Controllers
         {
 
 
-            var model = new NewTicketModel();
+            var model = new TicketReplyModel();
 
 
-            var results = await _emailBodyService.RenderBody("/Views/Emails/Ticket/NewTicket_mjml.cshtml", model);
+            var results = await _emailBodyService.RenderBody("/Views/Emails/Ticket/TicketReply_mjml.cshtml", model);
 
             return Content(results);
         }
@@ -51,19 +51,19 @@ namespace Harvest.Web.Controllers
         {
             var user = await _userService.GetCurrentUser();
             
-            var model = new NewTicketModel();
-            model.PI = user.NameAndEmail;
+            var model = new TicketReplyModel();
+            model.From = user.NameAndEmail;
             model.ProjectName = "Jason's Awesome Project";
             model.CreatedOn = DateTime.UtcNow.ToPacificTime().Date.Format("d");
-            model.DueDate = DateTime.UtcNow.AddYears(2).ToPacificTime().Date.Format("d");
-            model.Requirements =
-                "These are some Long requirements.These are some Long requirements.These are some Long requirements.These are some Long requirements.These are some Long requirements.These are some Long requirements.";
+
+            model.Reply =
+                "Make it so.";
             model.Subject = "Test subject";
             model.ButtonUrlForProject = "https://harvest.caes.ucdavis.edu/Fake2";
             model.ButtonUrlForTicket = "https://harvest.caes.ucdavis.edu/Fake1";
 
 
-            var emailBody = await _emailBodyService.RenderBody("/Views/Emails/Ticket/NewTicket.cshtml", model);
+            var emailBody = await _emailBodyService.RenderBody("/Views/Emails/Ticket/TicketReply.cshtml", model);
             await _notificationService.SendNotification(new string[] { user.Email }, emailBody, "A new Ticket has been create", "Harvest Notification - New Ticket");
 
             return Content("Done. Maybe. Well, possibly. If you don't get it, check the settings.");
