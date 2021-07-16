184279c2c858079310e8fb48b92429935e3bfd1b
Bryan M Wong <bmwong@ucdavis.edu>
2021-07-16 12:36:47 -0700

    Hid pagination if props.compact is true

--- Harvest.Web/ClientApp/src/Shared/ReactTable.tsx
+++ Harvest.Web/ClientApp/src/Shared/ReactTable.tsx
@@ -21,6 +21,7 @@ export const ReactTable = ({
   initialState,
   filterTypes,
   hideFilters = false,
+  hidePagination = false,
 }: any) => {
   const defaultColumn = React.useMemo(
     () => ({
@@ -33,6 +34,7 @@ export const ReactTable = ({
     getTableProps,
     getTableBodyProps,
     headerGroups,
+    rows,
     prepareRow,
     // pagination
     page,
@@ -103,91 +105,111 @@ export const ReactTable = ({
           )}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {page.map((row) => {
-            prepareRow(row);
-            return (
-              <tr className="rt-tr-group" {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
+          {/* This will reveal all rows if hidePagination is true */}
+          {hidePagination === true &&
+            rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr className="rt-tr-group" {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          {/* This will paginate the data if hidePagination is false */}
+          {hidePagination === false &&
+            page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr className="rt-tr-group" {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
         </tbody>
       </table>
-      <div className="pagination justify-content-center">
-        <PaginationItem
-          className="align-self-center"
-          onClick={() => gotoPage(0)}
-          disabled={!canPreviousPage}
-        >
-          <PaginationLink first />
-        </PaginationItem>
-        <PaginationItem
-          className="align-self-center"
-          onClick={() => previousPage()}
-          disabled={!canPreviousPage}
-        >
-          <PaginationLink previous />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink>
-            <span>
-              Page{" "}
-              <strong>
-                {pageIndex + 1} of {pageOptions.length}
-              </strong>{" "}
-            </span>
-            <span>
-              | Go to page:{" "}
-              <input
-                className="form-control d-inline"
-                type="number"
-                defaultValue={pageIndex + 1}
+      {hidePagination === true ? null : (
+        <div className="pagination justify-content-center">
+          <PaginationItem
+            className="align-self-center"
+            onClick={() => gotoPage(0)}
+            disabled={!canPreviousPage}
+          >
+            <PaginationLink first />
+          </PaginationItem>
+          <PaginationItem
+            className="align-self-center"
+            onClick={() => previousPage()}
+            disabled={!canPreviousPage}
+          >
+            <PaginationLink previous />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink>
+              <span>
+                Page{" "}
+                <strong>
+                  {pageIndex + 1} of {pageOptions.length}
+                </strong>{" "}
+              </span>
+              <span>
+                | Go to page:{" "}
+                <input
+                  className="form-control d-inline"
+                  type="number"
+                  defaultValue={pageIndex + 1}
+                  onChange={(e) => {
+                    const page = e.target.value
+                      ? Number(e.target.value) - 1
+                      : 0;
+                    gotoPage(page);
+                  }}
+                  style={{ width: "100px" }}
+                />
+              </span>{" "}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink>
+              <select
+                className="form-control"
+                value={pageSize}
                 onChange={(e) => {
-                  const page = e.target.value ? Number(e.target.value) - 1 : 0;
-                  gotoPage(page);
+                  ReactTableUtil.setPageSize(e.target.value);
+                  setPageSize(Number(e.target.value));
                 }}
-                style={{ width: "100px" }}
-              />
-            </span>{" "}
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink>
-            <select
-              className="form-control"
-              value={pageSize}
-              onChange={(e) => {
-                ReactTableUtil.setPageSize(e.target.value);
-                setPageSize(Number(e.target.value));
-              }}
-            >
-              {[10, 20, 30, 40, 50].map((pageSize) => (
-                <option key={pageSize} value={pageSize}>
-                  Show {pageSize}
-                </option>
-              ))}
-            </select>{" "}
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem
-          className="align-self-center"
-          onClick={() => nextPage()}
-          disabled={!canNextPage}
-        >
-          <PaginationLink next />
-        </PaginationItem>
-        <PaginationItem
-          className="align-self-center"
-          onClick={() => gotoPage(pageCount - 1)}
-          disabled={!canNextPage}
-        >
-          <PaginationLink last />
-        </PaginationItem>
-      </div>
+              >
+                {[10, 20, 30, 40, 50].map((pageSize) => (
+                  <option key={pageSize} value={pageSize}>
+                    Show {pageSize}
+                  </option>
+                ))}
+              </select>{" "}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem
+            className="align-self-center"
+            onClick={() => nextPage()}
+            disabled={!canNextPage}
+          >
+            <PaginationLink next />
+          </PaginationItem>
+          <PaginationItem
+            className="align-self-center"
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            <PaginationLink last />
+          </PaginationItem>
+        </div>
+      )}
     </>
   );
 };
