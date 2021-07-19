f3d7acc9ae909451dc6a10c0361c441d2249c5cf
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-07-19 08:34:33 -0700

    Ticket Attachment notification

---
+++ Harvest.Email/Models/Ticket/TicketAttachmentModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Harvest.Email.Models.Ticket
+{
+    public class TicketAttachmentModel
+    {
+        public string From { get; set; }
+        public string ProjectName { get; set; }
+        public string CreatedOn { get; set; }
+        public string Subject { get; set; }
+
+
+        public string[] AttachmentNames { get; set; }
+        public string ButtonUrlForTicket { get; set; }
+        public string ButtonUrlForProject { get; set; }
+    }
+}
