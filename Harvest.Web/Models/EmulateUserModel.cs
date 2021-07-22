c276351975c839ef81918d0d6f9e61885aff7b4b
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-07-22 10:37:05 -0700

    Emulation and access fix for rates
    Might work...

---
+++ Harvest.Web/Models/EmulateUserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Harvest.Web.Models
+{
+    public class EmulateUserViewModel
+    {
+        [Display(Name = "Search by Email")]
+        public string UserEmail { get; set; }
+    }
+}
