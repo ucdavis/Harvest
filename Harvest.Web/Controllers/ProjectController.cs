b0375d84571054d0e804bde5b128b42c31c34fdb
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2021-07-29 10:12:55 -0700

    Fix NRE and return 403 in ProjectController.Get

--- Harvest.Web/Controllers/ProjectController.cs
+++ Harvest.Web/Controllers/ProjectController.cs
@@ -95,17 +95,23 @@ namespace Harvest.Web.Controllers
                 .Include(p => p.Accounts)
                 .Include(p => p.PrincipalInvestigator)
                 .Include(p => p.CreatedBy)
-                .SingleOrDefaultAsync(p => p.Id == projectId && (hasAccess || p.PrincipalInvestigatorId == user.Id));
-            
+                .SingleOrDefaultAsync(p => p.Id == projectId);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            if (!hasAccess && project.PrincipalInvestigatorId != user.Id)
+            {
+                return Forbid();
+            }
+
             foreach (var file in project.Attachments) {
                 file.SasLink = fileService.GetDownloadUrl(storageSettings.ContainerName, file.Identifier).AbsoluteUri;
             }
 
-            if (project != null)
-            {
-                return Ok(project);
-            }
-            return NotFound();
+            return Ok(project);
         }
 
         [HttpGet]
