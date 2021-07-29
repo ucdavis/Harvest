e508519032a2305b4a9aba84553c83cdba59d6e2
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-07-29 14:14:19 -0700

    Cleanup (overloads)

--- Harvest.Core/Services/ProjectHistoryService.cs
+++ Harvest.Core/Services/ProjectHistoryService.cs
@@ -32,7 +32,7 @@ namespace Harvest.Core.Services
         Task<ProjectHistory> QuoteApproved(int projectId, IEnumerable<Account> accounts);
         Task<ProjectHistory> QuoteSaved(int projectId, Quote quote);
         Task<ProjectHistory> QuoteSubmitted(int projectId, Quote quote);
-        Task<ProjectHistory> RequestCreated( Project project);
+        Task<ProjectHistory> RequestCreated(Project project);
         Task<ProjectHistory> TicketCreated(int projectId, Ticket ticket);
         Task<ProjectHistory> TicketFilesAttached(int projectId, IEnumerable<TicketAttachment> attachments);
         Task<ProjectHistory> TicketNotesUpdated(int projectId, Ticket ticket);
@@ -78,16 +78,10 @@ namespace Harvest.Core.Services
             MakeHistory(projectId, nameof(QuoteSaved), quote.Text); // quote.Text is a preserialized QuoteDetail
         public Task<ProjectHistory> QuoteSubmitted(int projectId, Quote quote) =>
             MakeHistory(projectId, nameof(QuoteSubmitted), quote.Text); // quote.Text is a preserialized QuoteDetail
-
         public Task<ProjectHistory> RequestCreated(Project project) =>
-            MakeHistory2(project, nameof(RequestCreated), new ProjectHistoryModel(project));
-
-
-
+            MakeHistory(project, nameof(RequestCreated), new ProjectHistoryModel(project));
         public Task<ProjectHistory> QuoteApproved(int projectId, IEnumerable<Account> accounts) =>
             MakeHistory(projectId, nameof(QuoteApproved), accounts.Select(a => new AccountHistoryModel(a)));
-        public Task<ProjectHistory> RequestCreated(int projectId, Project project) =>
-            MakeHistory(projectId, nameof(RequestCreated), new ProjectHistoryModel(project));
         public Task<ProjectHistory> TicketCreated(int projectId, Ticket ticket) =>
             MakeHistory(projectId, nameof(TicketCreated), new TicketHistoryModel(ticket));
         public Task<ProjectHistory> TicketFilesAttached(int projectId, IEnumerable<TicketAttachment> attachments) =>
@@ -101,8 +95,8 @@ namespace Harvest.Core.Services
         private Task<ProjectHistory> MakeHistory(int projectId, string action, object detailsModel) =>
             MakeHistory(projectId, action, JsonSerializer.Serialize(detailsModel, _jsonOptions));
 
-        private Task<ProjectHistory> MakeHistory2(Project project, string action, object detailsModel) =>
-            MakeHistory2(project, action, JsonSerializer.Serialize(detailsModel, _jsonOptions));
+        private Task<ProjectHistory> MakeHistory(Project project, string action, object detailsModel) =>
+            MakeHistory(project, action, JsonSerializer.Serialize(detailsModel, _jsonOptions));
 
         private async Task<ProjectHistory> MakeHistory(int projectId, string action, string detailsSerialized)
         {
@@ -120,7 +114,7 @@ namespace Harvest.Core.Services
             return projectHistory;
         }
 
-        private async Task<ProjectHistory> MakeHistory2(Project project, string action, string detailsSerialized)
+        private async Task<ProjectHistory> MakeHistory(Project project, string action, string detailsSerialized)
         {
 
             var projectHistory = new ProjectHistory
