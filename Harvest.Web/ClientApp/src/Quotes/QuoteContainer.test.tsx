f95c088db45a69a05716d45e28bd4941d433e537
Bryan M Wong <bmwong@ucdavis.edu>
2021-08-03 16:26:40 -0700

    Fixed QuoteContainer

--- Harvest.Web/ClientApp/src/Quotes/QuoteContainer.test.tsx
+++ Harvest.Web/ClientApp/src/Quotes/QuoteContainer.test.tsx
@@ -4,7 +4,7 @@ import { MemoryRouter, Route } from "react-router-dom";
 import { act } from "react-dom/test-utils";
 
 import { QuoteContainer } from "./QuoteContainer";
-import { fakeAppContext, fakeProject, sampleRates } from "../Test/mockData";
+import { fakeAppContext, fakeProjectWithQuote, sampleRates } from "../Test/mockData";
 
 let container: Element;
 
@@ -55,7 +55,7 @@ describe("Quote Container", () => {
       const response = {
         status: 200,
         ok: true,
-        json: () => Promise.resolve(fakeProject),
+        json: () => Promise.resolve(fakeProjectWithQuote),
       };
 
       const response2 = {
