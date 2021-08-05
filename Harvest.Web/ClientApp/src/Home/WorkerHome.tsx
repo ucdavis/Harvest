2438957b15cef6b97531a2a6f70bd2bfce653bbc
Scott Kirkland <srkirkland@gmail.com>
2021-08-05 16:06:57 -0700

    get recently expensed projects for list

--- Harvest.Web/ClientApp/src/Home/WorkerHome.tsx
+++ Harvest.Web/ClientApp/src/Home/WorkerHome.tsx
@@ -1,18 +1,36 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-import AppContext from "../Shared/AppContext";
+import { Project } from "../types";
 
 export const WorkerHome = () => {
-  const userInfo = useContext(AppContext);
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const getProjectsWithRecentExpenses = async () => {
+      const response = await fetch("/expense/GetRecentExpensedProjects");
+      const projects: Project[] = await response.json();
+      setProjects(projects);
+    };
+
+    getProjectsWithRecentExpenses();
+  }, []);
 
   return (
     <>
       <h5>Quick Actions</h5>
       <ul className="list-group quick-actions">
         <li className="list-group-item">
-          <Link to="/expense/entry">Enter Expenses</Link>
+          <Link to="/expense/entry">Enter Expenses for Any Project</Link>
         </li>
+        {projects.map((project) => (
+          <li key={project.id} className="list-group-item">
+            <Link to={`/expense/entry/${project.id}`}>
+              Enter Expenses for {project.name}{" "}
+              <span className="badge badge-light">Recent</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
