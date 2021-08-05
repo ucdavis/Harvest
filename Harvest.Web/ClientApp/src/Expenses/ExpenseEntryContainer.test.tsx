68a616bcdf3e228a941fc825f9ba84b482312c0d
Bryan M Wong <bmwong@ucdavis.edu>
2021-08-05 11:41:02 -0700

    Created a test to check for rate changes

--- Harvest.Web/ClientApp/src/Expenses/ExpenseEntryContainer.test.tsx
+++ Harvest.Web/ClientApp/src/Expenses/ExpenseEntryContainer.test.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { MemoryRouter, Route } from "react-router-dom";
-import { act } from "react-dom/test-utils";
+import { act, Simulate } from "react-dom/test-utils";
 
 import { ExpenseEntryContainer } from "./ExpenseEntryContainer";
-import { fakeAppContext, fakeProjectWithQuote, sampleRates } from "../Test/mockData";
+import { fakeAppContext, sampleRates } from "../Test/mockData";
 
 let container: Element;
 
@@ -27,7 +27,17 @@ afterEach(() => {
 });
 
 describe("Expense Entry Container", () => {
+  const rateResponse = {
+    status: 200,
+    ok: true,
+    json: () => Promise.resolve(sampleRates),
+  };
+
   it("Generic Activity Default", async () => {
+    global.fetch = jest
+      .fn()
+      .mockImplementationOnce(() => Promise.resolve(rateResponse));
+
     await act(async () => {
       render(
         <MemoryRouter initialEntries={["/expense/entry/3"]}>
@@ -40,6 +50,29 @@ describe("Expense Entry Container", () => {
     });
 
     const defaultActivity = container.querySelector(".card-wrapper.mb-4.no-green");
-    expect(defaultActivity.textContent).toContain("Activity Name");
+    expect(defaultActivity?.textContent).toContain("Activity Name");
+  });
+
+  it("Rate Reflection", async () => {
+    await act(async () => {
+      global.fetch = jest
+        .fn()
+        .mockImplementationOnce(() => Promise.resolve(rateResponse));
+      
+      render(
+        <MemoryRouter initialEntries={["/expense/entry/3"]}>
+          <Route path="/expense/entry/:projectId">
+            <ExpenseEntryContainer />
+          </Route>
+        </MemoryRouter>,
+        container
+      );
+    });
+
+    const dropDown = container.querySelector("select");
+    const price = container.querySelector(".col-sm-2.offset-sm-1.col");
+    Simulate.change(dropDown, { target: { value: "3" }});
+
+    expect(price?.textContent).toContain("$60.00");
   });
 });
