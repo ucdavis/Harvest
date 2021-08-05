eb9fa89e01686abd560d2b7bc8dc962a815ce51a
Scott Kirkland <srkirkland@gmail.com>
2021-08-05 15:54:07 -0700

    simple supervisor dynamic home

--- Harvest.Web/ClientApp/src/Home/SupervisorHome.tsx
+++ Harvest.Web/ClientApp/src/Home/SupervisorHome.tsx
@@ -1,10 +1,21 @@
-import React, { useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-import AppContext from "../Shared/AppContext";
+import { Project } from "../types";
+import { StatusToActionRequired } from "../Util/MessageHelpers";
 
 export const SupervisorHome = () => {
-  const userInfo = useContext(AppContext);
+  const [projects, setProjects] = useState<Project[]>();
+
+  useEffect(() => {
+    // get info on projects requiring approval
+    const getProjects = async () => {
+      const response = await fetch("/project/RequiringManagerAttention");
+      const projects: Project[] = await response.json();
+      setProjects(projects);
+    };
+
+    getProjects();
+  }, []);
 
   return (
     <>
@@ -16,6 +27,26 @@ export const SupervisorHome = () => {
         <li className="list-group-item">
           <Link to="/expense/entry">Enter Expenses</Link>
         </li>
+        {projects !== undefined && projects.length > 0 && (
+          <li className="list-group-item">
+            <Link to={`/project/details/${projects[0].id}`}>
+              Quick jump to {projects[0].name}{" "}
+              <span className="badge badge-primary">
+                {StatusToActionRequired(projects[0].status)}
+              </span>
+            </Link>
+          </li>
+        )}
+        {projects !== undefined && projects.length > 1 && (
+          <li className="list-group-item">
+            <Link to={`/project/details/${projects[1].id}`}>
+              Quick jump to {projects[1].name}{" "}
+              <span className="badge badge-primary">
+                {StatusToActionRequired(projects[1].status)}
+              </span>
+            </Link>
+          </li>
+        )}
       </ul>
     </>
   );
