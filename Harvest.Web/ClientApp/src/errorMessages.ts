e7ade80ac8bb78489cd3dd5c39a1edfa22706ca8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-08-23 10:23:51 -0700

    Duplicate errorMessages in new file...

---
+++ Harvest.Web/ClientApp/src/errorMessages.ts
@@ -0,0 +1,6 @@
+export const ErrorMessages = {
+    "ActivityNameRequired" : "Activity name is required.",
+    "WorkItemUnit": "Work item time/unit must be a positive number.",
+    "WorkItemRate": "Work item rate must be a positive number.",
+    "WorkItemTotal": "Work item total must be a positive number.",
+}
