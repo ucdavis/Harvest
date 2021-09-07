62e58ca8433f249a4bcb268693b364b025ad879f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-09-07 11:25:41 -0700

    Require Requirements.
    Added a default value because it was nullable

---
+++ Harvest.Core/Migrations/SqlServer/20210907182145_ProjectRequirementsRequired.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Harvest.Core.Migrations.SqlServer
+{
+    public partial class ProjectRequirementsRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Requirements",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "N/A",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Requirements",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "N/A");
+        }
+    }
+}
