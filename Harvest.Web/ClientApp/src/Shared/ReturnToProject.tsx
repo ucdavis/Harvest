d490582581a9d8d3ca6daa8b16103b2669ff6ba9
Cal <cydoval@mac.com>
2021-09-08 16:29:07 -0600

    return to project page

---
+++ Harvest.Web/ClientApp/src/Shared/ReturnToProject.tsx
@@ -0,0 +1,12 @@
+import React, { useMemo, useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
+<div className="card-green-bg green-bg-border">
+  <div className="card-content">
+    <a href="#" className="btn btn-sm btn-link">
+      Back to project details <FontAwesomeIcon icon={faArrowLeft} />
+    </a>
+  </div>
+</div>;
