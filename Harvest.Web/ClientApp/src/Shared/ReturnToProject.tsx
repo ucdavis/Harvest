2a76ae087ffb67fe58b361e95ce47af243bcb846
Cal <cydoval@mac.com>
2021-09-08 16:40:51 -0600

    new component

--- Harvest.Web/ClientApp/src/Shared/ReturnToProject.tsx
+++ Harvest.Web/ClientApp/src/Shared/ReturnToProject.tsx
@@ -1,12 +1,20 @@
-import React, { useMemo, useState } from "react";
-
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-<div className="card-green-bg green-bg-border">
-  <div className="card-content">
-    <a href="#" className="btn btn-sm btn-link">
-      Back to project details <FontAwesomeIcon icon={faArrowLeft} />
-    </a>
+interface Props {
+  projectId: number;
+}
+
+export const ReturnToProject = (props: Props) => (
+  <div className="card-green-bg green-bg-border">
+    <div className="card-content">
+      <Link
+        to={`/project/details/${props.projectId}`}
+        className="btn btn-sm btn-link"
+      >
+        Back to project details <FontAwesomeIcon icon={faArrowLeft} />
+      </Link>
+    </div>
   </div>
-</div>;
+);
