ff2b28ac6e457ce3713d798a29cbbd46e6c7d89f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-09-14 11:57:57 -0700

    wip

---
+++ Test/TestsServices/InvoiceServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harvest.Core.Data;
+using Moq;
+using Xunit;
+
+namespace Test.TestsServices
+{
+    [Trait("Category", "ServiceTest")]
+    public class InvoiceServiceTests
+    {
+        public Mock<AppDbContext> MockDbContext { get; set; }
+    }
+}
