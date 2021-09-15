f62a45682f5aa8113a23a6afdf958f9a2354b5af
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-09-15 08:18:27 -0700

    Mocking

--- Test/TestsServices/InvoiceServiceTests.cs
+++ Test/TestsServices/InvoiceServiceTests.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Harvest.Core.Data;
+using Harvest.Core.Models.Settings;
+using Harvest.Core.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Moq;
+using Shouldly;
 using Xunit;
 
 namespace Test.TestsServices
@@ -13,5 +18,31 @@ namespace Test.TestsServices
     public class InvoiceServiceTests
     {
         public Mock<AppDbContext> MockDbContext { get; set; }
+        public Mock<IProjectHistoryService> MockProjectHistoryService { get; set; }
+        public Mock<IEmailService> MockEmailService { get; set; }
+        public Mock<IExpenseService> MockExpenseService { get; set; }
+        public Mock<IOptions<DevSettings>> MockDevSettings { get; set; }
+
+        public InvoiceServiceTests()
+        {
+            MockDbContext = new Mock<AppDbContext>(new DbContextOptions<AppDbContext>());
+            MockProjectHistoryService = new Mock<IProjectHistoryService>();
+            MockEmailService = new Mock<IEmailService>();
+            MockExpenseService = new Mock<IExpenseService>();
+            MockDevSettings = new Mock<IOptions<DevSettings>>();
+
+            var devSet = new DevSettings {RecreateDb = false, NightlyInvoices = true, UseSql = false};
+
+            MockDevSettings.Setup(a => a.Value).Returns(devSet);
+        }
+        [Fact]
+        public void TestSample()
+        {
+            var xxx = "1";
+            xxx.ShouldBe("1");
+
+            var invoiceServ = new InvoiceService(MockDbContext.Object, MockProjectHistoryService.Object, MockEmailService.Object,
+                MockExpenseService.Object, MockDevSettings.Object);
+        }
     }
 }
