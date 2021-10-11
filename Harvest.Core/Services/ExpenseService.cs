f9ee3229408b6a9c8d5949192b9737fb97433d2d
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-10-11 14:22:53 -0700

    Switch from monthly to yearly acreage Expenses
    resolve #613

--- Harvest.Core/Services/ExpenseService.cs
+++ Harvest.Core/Services/ExpenseService.cs
@@ -16,87 +16,96 @@ namespace Harvest.Core.Services
 {
     public interface IExpenseService
     {
-        Task CreateMonthlyAcreageExpense(Project project);
-        Task CreateAcreageExpense(int projectId, decimal amount);
+        Task CreateYearlyAcreageExpense(Project project);
+        Task CreateChangeRequestAdjustment(Project project, decimal extraAcres);
     }
 
     public class ExpenseService : IExpenseService
     {
         private readonly AppDbContext _dbContext;
         private readonly IProjectHistoryService _historyService;
-        private readonly IUserService _userService;
 
-        public ExpenseService(AppDbContext dbContext, IProjectHistoryService historyService, IUserService userService)
+        public ExpenseService(AppDbContext dbContext, IProjectHistoryService historyService)
         {
             _dbContext = dbContext;
             _historyService = historyService;
-            _userService = userService;
         }
 
-        public async Task CreateAcreageExpense(int projectId, decimal amount)
+
+        public async Task CreateYearlyAcreageExpense(Project project)
         {
-            if (amount < 0.01m)
-            {
-                throw new Exception("Amount must be >= 0.01");
-            }
-            var project = await _dbContext.Projects
-                .Include(p => p.AcreageRate)
-                .SingleAsync(p => p.Id == projectId);
-
-            var expense = await CreateExpense(project, amount);
-
-            await _dbContext.Expenses.AddAsync(expense);
-            await _historyService.AcreageExpenseCreated(project.Id, expense);
-            await _dbContext.SaveChangesAsync();
-        }
-
-        public async Task CreateMonthlyAcreageExpense(Project project)
-        {
-            // Don't want to continue running if there are no acres
             if (project.Acres <= 0)
             {
                 return;
             }
 
-            var amountToCharge = Math.Round((decimal)project.Acres * (project.AcreageRate.Price / 12), 2); //TODO: How to round this? I round to zero in the private method -- JS
+            //Look for an acreage expense in the last year or so.
+            var compareDate = DateTime.UtcNow.Date.AddYears(-1).AddDays(-1);
+            if (await _dbContext.Expenses.AnyAsync(a => a.ProjectId == project.Id && a.CreatedOn >= compareDate && a.Rate.Type == Rate.Types.Acreage))
+            {
+                Log.Information("Project {projectId} found an acreage expense within the last year, skipping", project.Id);
+                return;
+            }
+
+            var amountToCharge = Math.Round((decimal)project.Acres * project.AcreageRate.Price, 2, MidpointRounding.ToZero); //TODO: How to round this?
             if (amountToCharge < 0.01m)
             {
                 Log.Error("Project {projectId} would have an acreage amount less than 0.01. Skipping.", project.Id);
                 return;
             }
 
-            //Check for an unbilled acreage expense
-            if (await _dbContext.Expenses.AnyAsync(a =>
-                a.ProjectId == project.Id && a.InvoiceId == null && a.Rate.Type == Rate.Types.Acreage))
-            {
-                Log.Information("Project {projectId} Unbilled acreage expense already found. Skipping.", project.Id);
-                return;
-            }
-
-            var expense = await CreateExpense(project, amountToCharge);
+            var expense = CreateExpense(project, amountToCharge, (decimal)project.Acres);
 
             await _dbContext.Expenses.AddAsync(expense);
             await _historyService.AcreageExpenseCreated(project.Id, expense);
             await _dbContext.SaveChangesAsync();
-
         }
 
-        private async Task<Expense> CreateExpense(Project project, decimal amountToCharge)
+        public async Task CreateChangeRequestAdjustment(Project project, decimal extraAcres)
         {
-            var user = await _userService.GetCurrentUser();
+            if (extraAcres <= 0)
+            {
+                return;
+            }
+
+            if (project.AcreageRate == null) //I guess this is ok. Might want to call it later in the change request when it should have it?
+            {
+                var project1 = project;
+                project = await _dbContext.Projects.Include(a => a.AcreageRate).SingleAsync(a => a.Id == project1.Id);
+            }
+
+            var amountToCharge = Math.Round(extraAcres * project.AcreageRate.Price, 2, MidpointRounding.ToZero); //TODO: How to round this?
+            if (amountToCharge < 0.01m)
+            {
+                Log.Error("Adjustment Project {projectId} would have an acreage amount less than 0.01. Skipping.", project.Id);
+                return;
+            }
+
+            var expense = CreateExpense(project, amountToCharge, extraAcres);
+
+            await _dbContext.Expenses.AddAsync(expense);
+            await _historyService.AcreageExpenseCreated(project.Id, expense);
+            await _dbContext.SaveChangesAsync();
+        }
+
+
+        private Expense CreateExpense(Project project, decimal amountToCharge, decimal acres)
+        {
+            //If we populate the user, then this will have the PI's user because it is done in the request approval for change requests.
+
 
             var expense = new Expense
             {
                 Type = project.AcreageRate.Type,
                 Description = project.AcreageRate.Description,
-                Price = project.AcreageRate.Price / 12, //This can be more than 2 decimals
-                Quantity = (decimal) project.Acres,
+                Price = project.AcreageRate.Price,
+                Quantity = acres,
                 Total = Math.Round(amountToCharge,2, MidpointRounding.ToZero),
                 ProjectId = project.Id,
                 RateId = project.AcreageRate.Id,
                 InvoiceId = null,
                 CreatedOn = DateTime.UtcNow,
-                CreatedBy = user,
+                CreatedBy = null,
                 Account = project.AcreageRate.Account,
             };
             return expense;
