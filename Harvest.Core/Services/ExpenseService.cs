2ec15188e0673c39f68e0230d9d95495f08f4d38
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-10-18 13:33:01 -0700

    wip

--- Harvest.Core/Services/ExpenseService.cs
+++ Harvest.Core/Services/ExpenseService.cs
@@ -114,22 +114,21 @@ namespace Harvest.Core.Services
         {
             var now = _dateTimeService.DateTimeUtcNow();
             var compareDate = now.Date.AddYears(-1);
-            if (await _dbContext.Expenses.AnyAsync(a =>
-                a.ProjectId == project.Id && a.Type != Rate.Types.Adjustment && a.CreatedOn >= compareDate &&
-                a.Rate.Type == Rate.Types.Acreage))
+            if (await _dbContext.Expenses.AnyAsync(a => a.ProjectId == project.Id && a.Type != Rate.Types.Adjustment && a.CreatedOn >= compareDate && a.Rate.Type == Rate.Types.Acreage))
             {
+                Log.Information("Invoice adjustment not needed as an acreage expense wasn't found within the last year for project {id}", project.Id);
                 return;
             }
 
-            var rateChanged = false;
-            var partialRefund = false;
-
+            var rateChanged = false; //If true, we need to charge the new rate if there are any acres
+            var didWeDoAnything = false;
 
 
             //Ok, we found an acreage expense within the last year. Do we need to refund it?
             if (oldQuote.AcreageRateId != newQuote.AcreageRateId && oldQuote.AcreageRateId != null)
             {
                 //Create refund for old rate
+                Log.Information("Acreage expense refund created for Project {id}", project.Id);
                 rateChanged = true;
                 var oldRate = await _dbContext.Rates.SingleAsync(a => a.Id == oldQuote.AcreageRateId);
                 var amount = Math.Round((decimal)oldQuote.Acres * oldRate.Price, 2, MidpointRounding.ToZero) * -1; //Negative amount
@@ -138,6 +137,7 @@ namespace Harvest.Core.Services
                 refundExpense.Description = $"Acreage Adjustment (Refund) -- {oldRate.Description}".Truncate(250);
                 await _dbContext.Expenses.AddAsync(refundExpense);
                 await _historyService.AcreageExpenseCreated(project.Id, refundExpense);
+                didWeDoAnything = true;
             }
 
             if (!rateChanged)
@@ -150,12 +150,17 @@ namespace Harvest.Core.Services
                     var amount = Math.Round(acres * rate.Price, 2, MidpointRounding.ToZero); //Negative amount
                     if (amount < 0) //Hey maybe some rounding issue
                     {
-                        partialRefund = true;
+                        Log.Information("Creating new acreage refund expense because acres decreased. Project {id}", project.Id);
                         var refundExpense = CreateExpense(project, amount, acres, rate);
                         refundExpense.Type = Rate.Types.Adjustment;
                         refundExpense.Description = $"Acreage Adjustment (Partial Refund) -- {rate.Description}".Truncate(250);
                         await _dbContext.Expenses.AddAsync(refundExpense);
                         await _historyService.AcreageExpenseCreated(project.Id, refundExpense);
+                        didWeDoAnything = true;
+                    }
+                    else
+                    {
+                        Log.Error("Adjustment Project refund {projectId} would have an acreage amount greater than 0. Skipping.", project.Id);
                     }
                 }
             }
@@ -185,7 +190,7 @@ namespace Harvest.Core.Services
                     var amountToCharge = Math.Round(acres * project.AcreageRate.Price, 2, MidpointRounding.ToZero); //TODO: How to round this?
                     if (amountToCharge < 0.01m)
                     {
-                        Log.Error("Project {projectId} would have an acreage amount less than 0.01. Skipping.", project.Id);
+                        Log.Error("Adjustment Project  would have an acreage amount less than 0.01. Skipping. Project {id}", project.Id);
                         return;
                     }
 
@@ -195,14 +200,15 @@ namespace Harvest.Core.Services
 
                     await _dbContext.Expenses.AddAsync(expense);
                     await _historyService.AcreageExpenseCreated(project.Id, expense);
+                    didWeDoAnything = true;
                 }
             }
-            
 
+            if (didWeDoAnything)
+            {
+                await _dbContext.SaveChangesAsync();
+            }
 
-
-
-            throw new NotImplementedException();
         }
 
 
