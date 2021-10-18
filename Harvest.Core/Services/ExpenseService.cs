3f629f6d62248ff700f11641d59b49fadbba6b7d
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-10-18 11:14:53 -0700

    Refunds.
    WIP

--- Harvest.Core/Services/ExpenseService.cs
+++ Harvest.Core/Services/ExpenseService.cs
@@ -18,6 +18,7 @@ namespace Harvest.Core.Services
     {
         Task CreateYearlyAcreageExpense(Project project);
         Task CreateChangeRequestAdjustment(Project project, decimal extraAcres);
+        Task CreateChangeRequestAdjustmentMaybe(Project project, QuoteDetail newQuote, QuoteDetail oldQuote);
     }
 
     public class ExpenseService : IExpenseService
@@ -62,7 +63,7 @@ namespace Harvest.Core.Services
                 return;
             }
 
-            var expense = CreateExpense(project, amountToCharge, (decimal)project.Acres);
+            var expense = CreateExpense(project, amountToCharge, (decimal)project.Acres, project.AcreageRate);
 
             await _dbContext.Expenses.AddAsync(expense);
             await _historyService.AcreageExpenseCreated(project.Id, expense);
@@ -89,7 +90,7 @@ namespace Harvest.Core.Services
                 return;
             }
 
-            var expense = CreateExpense(project, amountToCharge, extraAcres);
+            var expense = CreateExpense(project, amountToCharge, extraAcres, project.AcreageRate);
             expense.Type = Rate.Types.Adjustment;
             expense.Description = $"Acreage Adjustment -- {expense.Description}".Truncate(250);
 
@@ -98,8 +99,69 @@ namespace Harvest.Core.Services
             await _dbContext.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Did an invoice get created that we have to do anything?
+        /// Did the rate Account change?
+        /// Did the acreage decrease?
+        /// Did the acreage increase?
+        /// Do we need to create
+        /// </summary>
+        /// <param name="project"></param>
+        /// <param name="newQuote"></param>
+        /// <param name="oldQuote"></param>
+        /// <returns></returns>
+        public async Task CreateChangeRequestAdjustmentMaybe(Project project, QuoteDetail newQuote, QuoteDetail oldQuote)
+        {
+            var rateChanged = false;
+            var partialRefund = false;
 
-        private Expense CreateExpense(Project project, decimal amountToCharge, decimal acres)
+            var now = _dateTimeService.DateTimeUtcNow();
+            var compareDate = now.Date.AddYears(-1);
+            if (await _dbContext.Expenses.AnyAsync(a => a.ProjectId == project.Id && a.Type != Rate.Types.Adjustment && a.CreatedOn >= compareDate && a.Rate.Type == Rate.Types.Acreage))
+            {
+                //Ok, we found an acreage expense within the last year. Do we need to refund it?
+                if (oldQuote.AcreageRateId != newQuote.AcreageRateId && oldQuote.AcreageRateId != null)
+                {
+                    //Create refund for old rate
+                    rateChanged = true;
+                    var oldRate = await _dbContext.Rates.SingleAsync(a => a.Id == oldQuote.AcreageRateId);
+                    var amount = Math.Round((decimal)oldQuote.Acres * oldRate.Price, 2, MidpointRounding.ToZero) * -1; //Negative amount
+                    var refundExpense = CreateExpense(project, amount, (decimal)oldQuote.Acres, oldRate);
+                    refundExpense.Type = Rate.Types.Adjustment;
+                    refundExpense.Description = $"Acreage Adjustment (Refund) -- {oldRate.Description}".Truncate(250);
+                    await _dbContext.Expenses.AddAsync(refundExpense);
+                    await _historyService.AcreageExpenseCreated(project.Id, refundExpense);
+                }
+
+                if (!rateChanged)
+                {
+                    //Do we need to do a partial refund?
+                    if (oldQuote.Acres > newQuote.Acres)
+                    {
+                        var rate = await _dbContext.Rates.SingleAsync(a => a.Id == newQuote.AcreageRateId);
+                        var acres = (decimal) (newQuote.Acres - oldQuote.Acres);
+                        var amount = Math.Round(acres * rate.Price, 2, MidpointRounding.ToZero); //Negative amount
+                        if (amount < 0) //Hey maybe some rounding issue
+                        {
+                            partialRefund = true;
+                            var refundExpense = CreateExpense(project, amount, acres, rate);
+                            refundExpense.Type = Rate.Types.Adjustment;
+                            refundExpense.Description = $"Acreage Adjustment (Partial Refund) -- {rate.Description}".Truncate(250);
+                            await _dbContext.Expenses.AddAsync(refundExpense);
+                            await _historyService.AcreageExpenseCreated(project.Id, refundExpense);
+                        }
+                    }
+                }
+            }
+
+
+
+
+            throw new NotImplementedException();
+        }
+
+
+        private Expense CreateExpense(Project project, decimal amountToCharge, decimal acres, Rate rate)
         {
             //If we populate the user, then this will have the PI's user because it is done in the request approval for change requests.
             var now = _dateTimeService.DateTimeUtcNow();
