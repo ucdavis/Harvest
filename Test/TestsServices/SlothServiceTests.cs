85d219590af211f0d9501580b2126a0cf756e4ca
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-10-22 13:42:52 -0700

    Using a passed client

--- Test/TestsServices/SlothServiceTests.cs
+++ Test/TestsServices/SlothServiceTests.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using Harvest.Core.Data;
 using Harvest.Core.Models.Settings;
 using Harvest.Core.Services;
@@ -7,9 +10,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Moq;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Harvest.Core.Domain;
 using Harvest.Core.Models.FinancialAccountModels;
+using Microsoft.AspNetCore.Http;
+using Moq.Protected;
 using Shouldly;
 using Test.Helpers;
 using TestHelpers.Helpers;
@@ -20,6 +26,7 @@ namespace Test.TestsServices
     [Trait("Category", "ServiceTest")]
     public class SlothServiceTests
     {
+        public Mock<HttpMessageHandler> MockMessageHandler { get; set; }
         public Mock<AppDbContext> MockDbContext { get; set; }
         public Mock<IEmailService> MockEmailService { get; set; }
         public Mock<IFinancialService> MockFinancialService { get; set; }
@@ -37,6 +44,7 @@ namespace Test.TestsServices
 
         public SlothServiceTests()
         {
+            MockMessageHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
             MockDbContext = new Mock<AppDbContext>(new DbContextOptions<AppDbContext>());
             MockProjectHistoryService = new Mock<IProjectHistoryService>();
             MockEmailService = new Mock<IEmailService>();
@@ -47,8 +55,25 @@ namespace Test.TestsServices
 
             JsonSerializerOptions = new JsonSerializerOptions();
 
+            MockMessageHandler.Protected().Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                )
+                // prepare the expected response of the mocked http call
+                .ReturnsAsync(new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent("{\r\n  \"id\": \"dfb39437-e604-4518-bc72-af89ea933684\",\r\n  \"status\": \"PendingApproval\",\r\n  \"sourceName\": \"Harvest Recharge\",\r\n  \"sourceType\": \"Recharge\",\r\n  \"merchantTrackingNumber\": \"1006\",\r\n  \"merchantTrackingUrl\": \"https://localhost:44308/Invoice/Details/6/1006\",\r\n  \"originCode\": \"CP\",\r\n  \"documentNumber\": \"000000254\",\r\n  \"documentType\": \"GLIB\",\r\n  \"kfsTrackingNumber\": \"0000000192\",\r\n  \"transactionDate\": \"2021-10-22T20:40:31.8700177Z\",\r\n  \"transfers\": [\r\n    {\r\n      \"id\": \"6dea5ce6-e2dc-4870-9ad9-08a7f47eb94a\",\r\n      \"amount\": 92.00,\r\n      \"chart\": \"3\",\r\n      \"account\": \"CRU9033\",\r\n      \"objectCode\": \"RAS5\",\r\n      \"description\": \"Proj: xxx Inv: 1006\",\r\n      \"direction\": \"Debit\",\r\n      \"fiscalYear\": 2022,\r\n      \"fiscalPeriod\": 4\r\n    },\r\n    {\r\n      \"id\": \"e56518c5-fb99-402d-ace7-4317d29aef94\",\r\n      \"amount\": 92.00,\r\n      \"chart\": \"3\",\r\n      \"account\": \"RRACRES\",\r\n      \"subAccount\": \"CNTRY\",\r\n      \"objectCode\": \"3900\",\r\n      \"description\": \"Proj: xxx Inv: 1006\",\r\n      \"direction\": \"Credit\",\r\n      \"fiscalYear\": 2022,\r\n      \"fiscalPeriod\": 4\r\n    }\r\n  ],\r\n  \"isReversal\": false,\r\n  \"hasReversal\": false,\r\n  \"statusEvents\": [\r\n    {\r\n      \"id\": \"3efaebae-a2de-4dc1-8be7-2cef5f9624fc\",\r\n      \"status\": \"PendingApproval\",\r\n      \"eventDate\": \"2021-10-22T20:40:45.5049293Z\",\r\n      \"eventDetails\": \"File: TransactionsController.cs, Member: Post, Line: 211\",\r\n      \"transactionId\": \"dfb39437-e604-4518-bc72-af89ea933684\"\r\n    }\r\n  ]\r\n}"), //Change to sloth content
+                })
+                .Verifiable();
+            var httpClient = new HttpClient(MockMessageHandler.Object)
+            {
+                BaseAddress = new Uri("http://test.com/"),
+            };
+
             SlothService = new SlothService(MockDbContext.Object, MockSlothSettings.Object, MockFinancialService.Object,
-                JsonSerializerOptions, MockProjectHistoryService.Object, MockEmailService.Object);
+                JsonSerializerOptions, MockProjectHistoryService.Object, MockEmailService.Object, httpClient);
         }
 
         private void SetupGenericData()
@@ -86,6 +111,20 @@ namespace Test.TestsServices
             KfsAccount rtval = new KfsAccount();
             MockFinancialService.Setup(a => a.Parse(It.IsAny<string>()))
                 .Callback<string>(a => rtval = realFinancialService.Parse(a)).Returns(() => rtval);
+
+
+            var isValidRtn = new AccountValidationModel();
+            MockFinancialService.Setup(a => a.IsValid(It.IsAny<string>()))
+                .Callback<string>(a => rtval = realFinancialService.Parse(a)).Returns(async () => await SetValidAccount(rtval));
+
+        }
+
+        private Task<AccountValidationModel> SetValidAccount(KfsAccount kfsAccount)
+        {
+            var rtValue = new AccountValidationModel();
+            rtValue.KfsAccount = kfsAccount;
+            rtValue.IsValid = true;
+            return Task.FromResult(rtValue);
         }
 
         [Theory]
@@ -150,7 +189,7 @@ namespace Test.TestsServices
             var rtValue = await SlothService.MoveMoney(invoice.Id);
             rtValue.ShouldNotBeNull();
             rtValue.IsError.ShouldBeTrue();
-            rtValue.Message.ShouldBe($"No expenses found for invoice: {invoice.Id}");
+            rtValue.Message.ShouldBe($"No Transfers Generated for invoice: {invoice.Id}");
         }
 
 
