50c686bd493427f60253d32992485e1f7fe99ff8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-10-29 11:04:25 -0700

    prettier

--- Harvest.Web/ClientApp/src/Projects/ProjectUnbilledButton.tsx
+++ Harvest.Web/ClientApp/src/Projects/ProjectUnbilledButton.tsx
@@ -4,8 +4,8 @@ import { formatCurrency } from "../Util/NumberFormatting";
 import { useIsMounted } from "../Shared/UseIsMounted";
 
 interface Props {
-    projectId: number;
-    remaining: number;
+  projectId: number;
+  remaining: number;
 }
 
 export const ProjectUnbilledButton = (props: Props) => {
@@ -35,22 +35,24 @@ export const ProjectUnbilledButton = (props: Props) => {
     );
   }
   if (total === undefined || (total !== 0 && total <= props.remaining)) {
-      return (
-          <Link
-              to={`/expense/unbilled/${props.projectId}`}
-              className="btn btn-lg btn-light">
-              View Unbilled Expenses - $
-              {total === undefined ? "xx.xx" : formatCurrency(total)}
-          </Link>
-      );
+    return (
+      <Link
+        to={`/expense/unbilled/${props.projectId}`}
+        className="btn btn-lg btn-light"
+      >
+        View Unbilled Expenses - $
+        {total === undefined ? "xx.xx" : formatCurrency(total)}
+      </Link>
+    );
   }
 
   return (
-      <Link
-          to={`/expense/unbilled/${props.projectId}`}
-          className="btn btn-lg  alert-danger">
-          View Unbilled Expenses - $
-          {total === undefined ? "xx.xx" : formatCurrency(total)}
-      </Link>
+    <Link
+      to={`/expense/unbilled/${props.projectId}`}
+      className="btn btn-lg  alert-danger"
+    >
+      View Unbilled Expenses - $
+      {total === undefined ? "xx.xx" : formatCurrency(total)}
+    </Link>
   );
 };
