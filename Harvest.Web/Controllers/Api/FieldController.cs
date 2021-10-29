3093a9bc2f79722d12c32d038512a20914f94cf3
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-10-29 11:44:39 -0700

    hide orig fields

--- Harvest.Web/Controllers/Api/FieldController.cs
+++ Harvest.Web/Controllers/Api/FieldController.cs
@@ -20,15 +20,18 @@ namespace Harvest.Web.Controllers.Api
         }
 
         // Get all fields that are part of projects active between the given dates
-        public async Task<IActionResult> Active(DateTime start, DateTime end)
+        public async Task<IActionResult> Active(DateTime start, DateTime end, int projectId)
         {
+            var project = await _dbContext.Projects.SingleAsync(a => a.Id == projectId);
+
             // TODO: NOTE - need to convert Location to Geometry since we store it in the db as Location even though GeoJSON would like the name to be "geometry"
             // overlapping projects are those that start before the given project ends, and ends after the given project starts
             var fields = await _dbContext.Fields
-                .Where(f => f.Project.IsApproved && (f.Project.End >= start && f.Project.Start <= end))
+                .Where(f =>f.ProjectId != project.OriginalProjectId && f.Project.IsApproved && (f.Project.End >= start && f.Project.Start <= end))
                 .AsNoTracking().Select(f => new { f.Id, f.Crop, Geometry = f.Location }).ToArrayAsync();
 
             return Json(fields);
+
         }
     }
 }
