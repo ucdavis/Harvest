5d0a24a6b36987a64d6b289e9ae730a70d52efba
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-11-02 10:50:22 -0700

    PI Only policy

--- Harvest.Web/Startup.cs
+++ Harvest.Web/Startup.cs
@@ -112,6 +112,8 @@ namespace Harvest.Web
                     new VerifyRoleAccess(AccessConfig.GetRoles(AccessCodes.WorkerAccess))));
                 options.AddPolicy(AccessCodes.PrincipalInvestigator, policy => policy.Requirements.Add(
                     new VerifyRoleAccess(AccessConfig.GetRoles(AccessCodes.PrincipalInvestigator))));
+                options.AddPolicy(AccessCodes.PiOnly, policy => policy.Requirements.Add(
+                    new VerifyRoleAccess(AccessConfig.GetRoles(AccessCodes.PiOnly))));
             });
 
             services.AddScoped<IAuthorizationHandler, VerifyRoleAccessHandler>();
