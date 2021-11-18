e1519d5ddedde679bae85d1075184a4e3f7da775
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-11-18 11:13:34 -0800

    Ping controller

---
+++ Harvest.Web/Controllers/Api/PingController.cs
@@ -0,0 +1,28 @@
+﻿using Harvest.Core.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Harvest.Web.Controllers.Api
+{
+
+    public class PingController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public PingController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [Route("api/ping")]
+        public async Task<ActionResult> Index()
+        {
+            if (await _dbContext.Database.CanConnectAsync())
+            {
+                return Content("Ping");
+            }
+
+            return BadRequest();
+
+        }
+    }
+}
