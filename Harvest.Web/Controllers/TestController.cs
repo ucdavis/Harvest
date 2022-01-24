9c1d4b88d7f1c044d1c319eb8b793164f67a211e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-01-24 09:44:07 -0800

    method for test invoice errors

--- Harvest.Web/Controllers/TestController.cs
+++ Harvest.Web/Controllers/TestController.cs
@@ -31,11 +31,11 @@ namespace Harvest.Web.Controllers
         }
         public async Task<IActionResult> TestBody()
         {
-            var model = new InvoiceEmailModel();
+            var model = new InvoiceErrorModel();
 
 
 
-            var results = await RazorTemplateEngine.RenderAsync("/Views/Emails/Invoice/InvoiceCreated_mjml.cshtml", model);
+            var results = await RazorTemplateEngine.RenderAsync("/Views/Emails/Invoice/InvoiceErrors_mjml.cshtml", model);
 
             return Content(results);
         }
@@ -139,6 +139,13 @@ namespace Harvest.Web.Controllers
         {
             var invoice = await _dbContext.Invoices.SingleAsync(a => a.Id == 4);
             var rtValue = await _emailService.InvoiceDone(invoice, "Cancelled");
+            return Content($"Email was {rtValue}");
+        }
+
+        public async Task<IActionResult> TestInvoiceErrors()
+        {
+            var invoice = await _dbContext.Invoices.SingleAsync(a => a.Id == 1);
+            var rtValue = await _emailService.InvoiceError(invoice);
             return Content($"Email was {rtValue}");
         }
     }
