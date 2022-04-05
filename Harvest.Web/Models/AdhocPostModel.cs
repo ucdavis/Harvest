ef56ba872aa2a3bc5cdd98fc49422bd70a6bd919
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-04-05 09:19:06 -0700

    Create a post model

---
+++ Harvest.Web/Models/AdhocPostModel.cs
@@ -0,0 +1,10 @@
+﻿using Harvest.Core.Domain;
+
+namespace Harvest.Web.Models
+{
+    public class AdhocPostModel
+    {
+        public Project Project { get;set;}
+        public Expense[] Expenses { get;set;}
+    }
+}
