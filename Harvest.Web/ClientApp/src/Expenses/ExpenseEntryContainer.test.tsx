84b84b6da0371a689647a940f02d278ef012205d
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-04-12 10:39:43 -0700

    Ugly fix for test

--- Harvest.Web/ClientApp/src/Expenses/ExpenseEntryContainer.test.tsx
+++ Harvest.Web/ClientApp/src/Expenses/ExpenseEntryContainer.test.tsx
@@ -29,7 +29,7 @@ beforeEach(() => {
   global.fetch = jest.fn().mockImplementation((x) =>
     responseMap(x, {
       "/api/Project": projectResponse,
-      "/api/Rate/Active": rateResponse,
+      "/api/undefined/Rate/Active": rateResponse,
     })
   );
 });
