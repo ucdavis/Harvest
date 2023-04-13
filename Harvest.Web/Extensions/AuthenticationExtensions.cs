fe5c73f8d2dc52fcf3b99f4e20d1e7e572d0ffbd
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-04-13 14:06:00 -0700

    Cleanup

--- Harvest.Web/Extensions/AuthenticationExtensions.cs
+++ Harvest.Web/Extensions/AuthenticationExtensions.cs
@@ -20,26 +20,23 @@ namespace Harvest.Web.Extensions
             {
                 teamSlug = context.Request.Path.Value.Split('/')[1];
             }
-            catch (System.Exception ex)
+            catch (System.Exception)
             {
-                teamSlug = null;
+                //Swallow it
             }
-            if (string.IsNullOrWhiteSpace(teamSlug))
-            {
-                teamSlug = null;
-            }
+
             var user = context.User.GetUserInfo();
 
             var dbContext = context.RequestServices.GetRequiredService<AppDbContext>();
 
             IEnumerable<string> roles;
-            if (teamSlug != null)
+            if (!string.IsNullOrWhiteSpace(teamSlug))
             {
-
                 roles = await dbContext.Permissions.Where(p => p.User.Iam == user.Iam && (p.Team == null || p.Team.Slug == teamSlug)).Select(p => p.Role.Name).ToArrayAsync();
             }
             else
             {
+                //When there is no team, we want to get all roles so we can show them the team picker. (PI's never need to pick teams until they create a project)
                 roles = await dbContext.Permissions.Where(p => p.User.Iam == user.Iam).Select(p => p.Role.Name).ToArrayAsync();
             }
             if (await dbContext.Projects.AnyAsync(p => p.PrincipalInvestigator.Iam == user.Iam))
