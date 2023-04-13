17cbdaf9fd11193e0e5547c5f51e7a447468b47d
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-04-13 12:33:38 -0700

    Don't really like this

--- Harvest.Web/Extensions/AuthenticationExtensions.cs
+++ Harvest.Web/Extensions/AuthenticationExtensions.cs
@@ -12,14 +12,36 @@ namespace Harvest.Web.Extensions
 {
     public static class AuthenticationExtensions
     {
+
         public static async Task<string> GetUserRoles(this HttpContext context)
         {
+            string teamSlug = null;
+            try
+            {
+                teamSlug = context.Request.Path.Value.Split('/')[1];
+            }
+            catch (System.Exception ex)
+            {
+                teamSlug = null;
+            }
+            if (string.IsNullOrWhiteSpace(teamSlug))
+            {
+                teamSlug = null;
+            }
             var user = context.User.GetUserInfo();
 
             var dbContext = context.RequestServices.GetRequiredService<AppDbContext>();
 
-            IEnumerable<string> roles = await dbContext.Permissions.Where(p => p.User.Iam == user.Iam).Select(p => p.Role.Name).ToArrayAsync();
+            IEnumerable<string> roles;
+            if (teamSlug != null)
+            {
 
+                roles = await dbContext.Permissions.Where(p => p.User.Iam == user.Iam && (p.Team == null || p.Team.Slug == teamSlug)).Select(p => p.Role.Name).ToArrayAsync();
+            }
+            else
+            {
+                roles = await dbContext.Permissions.Where(p => p.User.Iam == user.Iam).Select(p => p.Role.Name).ToArrayAsync();
+            }
             if (await dbContext.Projects.AnyAsync(p => p.PrincipalInvestigator.Iam == user.Iam))
             {
                 roles = roles.Append(Role.Codes.PI);
@@ -28,7 +50,8 @@ namespace Harvest.Web.Extensions
             return JsonSerializer.Serialize(roles);
         }
 
-        public static string GetUserDetails(this HttpContext context) {
+        public static string GetUserDetails(this HttpContext context)
+        {
             var user = context.User.GetUserInfo();
 
             return JsonSerializer.Serialize(user, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
