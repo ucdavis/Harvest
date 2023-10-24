621d0b2cb216756a30aa72f023025626e6a99aba
River Holstege <laholstege@ucdavis.edu>
2023-10-24 15:51:42 -0700

    update internal state if parent changes itself

--- Harvest.Web/ClientApp/src/Shared/StatefulInput.tsx
+++ Harvest.Web/ClientApp/src/Shared/StatefulInput.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input, InputProps } from "reactstrap";
 
 interface IProps extends InputProps {
@@ -11,6 +11,17 @@ const StatefulInput = (props: IProps) => {
   const [internalValue, setInternalValue] = useState(
     props.value.toString() ?? ""
   );
+
+  useEffect(() => {
+    // if parent changes value, update our internal value
+    // this could happen on a slow load, or if the parent zero's out the value
+    // this is not best practices for how to handle state (should have one source of truth)
+    // but it's the easiest way to have a string input for our number field (for floats) and not change the type
+    if (props.value.toString() !== internalValue) {
+      setInternalValue(props.value.toString());
+    }
+  }, [props.value, internalValue]);
+
   return (
     <Input
       {...props}
