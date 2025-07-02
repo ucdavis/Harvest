e97196d931da03dc0de8392704836ab7eb7772f5
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-07-02 13:25:32 -0700

    Show projects you have access to based on ProjectPermissions
    Issue #917

--- Harvest.Web/Controllers/Api/ProjectController.cs
+++ Harvest.Web/Controllers/Api/ProjectController.cs
@@ -127,11 +127,20 @@ namespace Harvest.Web.Controllers.Api
         {
             var user = await _userService.GetCurrentUser();
 
+            //Look for any projects where I have a project permission
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            //Just for testing, remove this later as the call below will do the same thing
+            //var projectsWithPermissions = await _dbContext.ProjectPermissions.Where(a => a.UserId == user.Id).Select(a => a.ProjectId).ToArrayAsync();
+
             // TODO: only show projects where between start and end?
             return Ok(await _dbContext.Projects
                 .Include(p => p.PrincipalInvestigator)
                 .Include(p => p.Team)
-                .Where(p => p.IsActive && p.PrincipalInvestigatorId == user.Id)
+                .Where(p => p.IsActive && (p.PrincipalInvestigatorId == user.Id || p.ProjectPermissions.Any(a => a.UserId == user.Id)))
                 .ToArrayAsync());
         }
 
