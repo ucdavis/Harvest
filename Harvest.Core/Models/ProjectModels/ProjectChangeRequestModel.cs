25a89b723f204dc3b6908cfb56e67873d4e313cb
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-07-17 08:03:16 -0700

    projection

---
+++ Harvest.Core/Models/ProjectModels/ProjectChangeRequestModel.cs
@@ -0,0 +1,23 @@
+﻿using Harvest.Core.Domain;
+using System;
+using System.Linq.Expressions;
+
+namespace Harvest.Core.Models.ProjectModels
+{
+    public class ProjectChangeRequestModel
+    {
+        public int Id { get;set; }
+        public string Status { get; set; } 
+        public string Name { get; set; }
+
+        public static Expression<Func<Project, ProjectChangeRequestModel>> Projection()
+        {
+            return a => new ProjectChangeRequestModel
+            {
+                Id = a.Id,
+                Status = a.Status,
+                Name = a.Name
+            };
+        }
+    }
+}
