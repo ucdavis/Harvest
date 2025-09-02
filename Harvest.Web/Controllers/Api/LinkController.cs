33d6a1aaaef289151cd279b4ebb51252f723f41e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-09-02 08:45:30 -0700

    Adds API endpoint for link generation
    Implements an API endpoint that allows authenticated workers to generate an API key based on their permissions,
    providing a secure method for accessing authorized resources.
    
    The endpoint checks the user's role and team affiliation to ensure they have the necessary permissions before generating the key.

---
+++ Harvest.Web/Controllers/Api/LinkController.cs
@@ -0,0 +1,44 @@
+﻿using Harvest.Core.Data;
+using Harvest.Core.Domain;
+using Harvest.Core.Models;
+using Harvest.Core.Services;
+using Harvest.Web.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Harvest.Web.Controllers.Api
+{
+    [Authorize]
+    public class LinkController : SuperController
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IUserService _userService;
+        private readonly IApiKeyService _apiKeyService;
+        public LinkController(AppDbContext dbContext, IUserService userService, IApiKeyService apiKeyService)
+        {
+            _dbContext = dbContext;
+            _userService = userService;
+            _apiKeyService = apiKeyService;
+        }
+
+        [HttpGet]
+        [Authorize(Policy = AccessCodes.WorkerAccess)]
+        public async Task<IActionResult> Get()
+        {
+            var user = await _userService.GetCurrentUser();
+            var permission = await _dbContext.Permissions
+                .Where(p => p.UserId == user.Id && p.Team.Slug == TeamSlug && p.Role.Name == Role.Codes.Worker)
+                .SingleOrDefaultAsync();
+            if (permission == null)
+            {
+                return NotFound();
+            }
+            var apiKey = await _apiKeyService.GenerateApiKeyAsync(permission.Id);
+
+            return Ok(apiKey);
+        }
+    }
+}
