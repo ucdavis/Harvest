ae2bfd0d172c296275f0a3014f78011e23e596b6
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-09-11 11:59:56 -0700

    Allows null CreatedDate
    Updates the CreatedDate property in the CreateExpenseResultsModel to be nullable.
    This resolves a potential issue where a value may not always be available, preventing errors.

--- Harvest.Web/Models/MobileModels/CreateExpenseResultsModel.cs
+++ Harvest.Web/Models/MobileModels/CreateExpenseResultsModel.cs
@@ -15,7 +15,7 @@ namespace Harvest.Web.Models.MobileModels
     {
         public Guid? WorkerMobileId { get; set; }
         public int? ExpenseId { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
         public string Result { get; set; }
         public CreateExpenseErrors Errors { get; set; }
 
